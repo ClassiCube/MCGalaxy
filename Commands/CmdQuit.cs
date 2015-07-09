a96c60a8aa35e609cbcbb209af654d0fb19424c8
Fire200055 <ane200055@gmail.com>
2015-07-09 20:54:43 +0100

    Added a quit command. How can you not have one? :P

---
+++ Commands/CmdQuit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCGalaxy.Commands
+{
+    public sealed class CmdQuit : Command
+    {
+        public override string name { get { return "quit"; } }
+        public override string shortcut { get { return ""; } }
+        public override string type { get { return "other"; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+        public CmdQuit() { }
+
+        public override void Use(Player p, string message)
+        {
+            if (message != "")
+            {
+                p.Kick("Left the game: " + message);
+            }
+            else
+            {
+                p.Kick("Left the game.");
+            }
+        }
+        public override void Help(Player p)
+        {
+            Player.SendMessage(p, "/quit <reason> - Leave the server.");
+        }
+    }
+}
