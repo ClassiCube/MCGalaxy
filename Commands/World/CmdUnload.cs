798bbab2f7337de3a2010d0f140dc8f105eff885
UnknownShadow200 <unknownshadow200@gmail.com>
2015-12-07 08:55:42 +1100

    Cleanup /unload.

--- Commands/World/CmdUnload.cs
+++ Commands/World/CmdUnload.cs
@@ -1,19 +1,19 @@
 /*
-	Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-	
-	Dual-licensed under the	Educational Community License, Version 2.0 and
-	the GNU General Public License, Version 3 (the "Licenses"); you may
-	not use this file except in compliance with the Licenses. You may
-	obtain a copy of the Licenses at
-	
-	http://www.opensource.org/licenses/ecl2.php
-	http://www.gnu.org/licenses/gpl-3.0.html
-	
-	Unless required by applicable law or agreed to in writing,
-	software distributed under the Licenses are distributed on an "AS IS"
-	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-	or implied. See the Licenses for the specific language governing
-	permissions and limitations under the Licenses.
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
+    
+    Dual-licensed under the    Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
 */
 namespace MCGalaxy.Commands
 {
@@ -21,42 +21,40 @@ namespace MCGalaxy.Commands
     {
         public override string name { get { return "unload"; } }
         public override string shortcut { get { return ""; } }
-       public override string type { get { return CommandTypes.World; } }
+        public override string type { get { return CommandTypes.World; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public CmdUnload() { }
 
-        public override void Use(Player p, string message)
-        {
-            if (message.ToLower() == "empty")
-            {
-                Server.levels.ForEach(l =>
-                {
+        public override void Use(Player p, string message) {
+            string name = message.ToLower();
+            if (name == "" && p == null) {
+                Player.SendMessage(p, "You must specify a map name when unloading from console."); return;
+            }
+            
+            if (name == "") {
+                if (!p.level.Unload())
+                    Player.SendMessage(p, "You cannot unload this level.");
+            } else if (name == "empty") {
+                Server.levels.ForEach(l => {
                     if (l.players.Count <= 0 && l != Server.mainLevel)
                         l.Unload(true, true);
                 });
-                return;
+            } else {
+                Level level = Level.Find(name);
+                if (level == null) {
+                    Player.SendMessage(p, "There is no level \"" + name + "\" loaded.");
+                } else if (!level.Unload()) {
+                    Player.SendMessage(p, "You cannot unload this level.");
+                }
             }
-
-            Level level = Level.Find(message);
-            if (level != null)
-            {
-                if (!level.Unload()) Player.SendMessage(p, "You cannot unload the main level.");
-                return;
-            }
-            if (message == "")
-            {
-                level = p.level;
-                level.Unload();
-                return;
-            }
-
-            Player.SendMessage(p, "There is no level \"" + message + "\" loaded.");
         }
-        public override void Help(Player p)
-        {
-            Player.SendMessage(p, "/unload [level] - Unloads a level.");
-            Player.SendMessage(p, "/unload empty - Unloads an empty level.");
+        
+        public override void Help(Player p) {
+            Player.SendMessage(p, "%a/unload [map name]");
+            Player.SendMessage(p, "%eUnloads the given map.");
+            Player.SendMessage(p, "%e  If map name is \"empty\", unloads all maps with no players in them.");
+            Player.SendMessage(p, "%e  If no map name is given, unloads the map you are currently in."); 
         }
     }
 }
