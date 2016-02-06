614dc24508edfd224eb774689cfa689e5f05a521
UnknownShadow200 <unknownshadow200@gmail.com>
2016-02-06 21:11:33 +1100

    Cleanup the code of /click and /roll.

--- Commands/building/CmdClick.cs
+++ Commands/building/CmdClick.cs
@@ -1,24 +1,24 @@
 /*
-	Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-	
-	Dual-licensed under the	Educational Community License, Version 2.0 and
-	the GNU General Public License, Version 3 (the "Licenses"); you may
-	not use this file except in compliance with the Licenses. You may
-	obtain a copy of the Licenses at
-	
-	http://www.opensource.org/licenses/ecl2.php
-	http://www.gnu.org/licenses/gpl-3.0.html
-	
-	Unless required by applicable law or agreed to in writing,
-	software distributed under the Licenses are distributed on an "AS IS"
-	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-	or implied. See the Licenses for the specific language governing
-	permissions and limitations under the Licenses.
-*/
-namespace MCGalaxy.Commands
-{
-    public sealed class CmdClick : Command
-    {
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
+    
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
+ */
+namespace MCGalaxy.Commands {
+    
+    public sealed class CmdClick : Command {
+        
         public override string name { get { return "click"; } }
         public override string shortcut { get { return "x"; } }
         public override string type { get { return CommandTypes.Building; } }
@@ -26,32 +26,21 @@ namespace MCGalaxy.Commands
         public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
         public CmdClick() { }
 
-        public override void Use(Player p, string message)
-        {
+        public override void Use(Player p, string message) {
             if (p == null) { MessageInGameOnly(p); return; }
-            string[] parameters = message.Split(' ');
             ushort[] click = p.lastClick;
 
-            if (message.IndexOf(' ') != -1)
-            {
-                if (parameters.Length != 3)
-                {
-                    Help(p);
-                    return;
-                }
-                else
-                {
-                    for (int value = 0; value < 3; value++)
-                    {
-                        if (parameters[value].ToLower() == "x" || parameters[value].ToLower() == "y" || parameters[value].ToLower() == "z")
-                            click[value] = p.lastClick[value];
-                        else if (isValid(parameters[value], value, p))
-                            click[value] = ushort.Parse(parameters[value]);
-                        else
-                        {
-                            Player.SendMessage(p, "\"" + parameters[value] + "\" was not valid");
-                            return;
-                        }
+            if (message.IndexOf(' ') != -1) {
+                string[] args = message.ToLower().Split(' ');
+                if (args.Length != 3) { Help(p); return; }
+                
+                for (int i = 0; i < 3; i++) {
+                    if (args[i] == "x" || args[i] == "y" || args[i] == "z") {
+                        click[i] = p.lastClick[i];
+                    } else if (IsValid(p, i, args[i])) {
+                        click[i] = ushort.Parse(args[i]);
+                    } else {
+                        Player.SendMessage(p, "\"" + args[i] + "\" was not valid");  return;
                     }
                 }
             }
@@ -60,28 +49,21 @@ namespace MCGalaxy.Commands
             p.ManualChange(click[0], click[1], click[2], 0, Block.rock);
             Player.SendMessage(p, "Clicked &b(" + click[0] + ", " + click[1] + ", " + click[2] + ")");
         }
-        public override void Help(Player p)
-        {
+
+        bool IsValid(Player p, int axis, string message) {
+            ushort value;
+            if (!ushort.TryParse(message, out value)) return false;
+
+            if (value >= p.level.Width && axis == 0) return false;
+            else if (value >= p.level.Height && axis == 1) return false;
+            else if (value >= p.level.Length && axis == 2) return false;
+            return true;
+        }
+        
+        public override void Help(Player p) {
             Player.SendMessage(p, "/click [x y z] - Fakes a click");
             Player.SendMessage(p, "If no xyz is given, it uses the last place clicked");
             Player.SendMessage(p, "/click 200 y 200 will cause it to click at 200x, last y and 200z");
-        }
-
-        private bool isValid(string message, int dimension, Player p)
-        {
-            ushort testValue;
-            try {
-                testValue = ushort.Parse(message);
-            } catch { return false; }
-
-            if (testValue < 0)
-                return false;
-
-            if (testValue >= p.level.Width && dimension == 0) return false;
-            else if (testValue >= p.level.Height && dimension == 1) return false;
-            else if (testValue >= p.level.Length && dimension == 2) return false;
-
-            return true;
         }
     }
 }
--- Commands/other/CmdRoll.cs
+++ Commands/other/CmdRoll.cs
@@ -1,25 +1,25 @@
 /*
-	Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-	
-	Dual-licensed under the	Educational Community License, Version 2.0 and
-	the GNU General Public License, Version 3 (the "Licenses"); you may
-	not use this file except in compliance with the Licenses. You may
-	obtain a copy of the Licenses at
-	
-	http://www.opensource.org/licenses/ecl2.php
-	http://www.gnu.org/licenses/gpl-3.0.html
-	
-	Unless required by applicable law or agreed to in writing,
-	software distributed under the Licenses are distributed on an "AS IS"
-	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-	or implied. See the Licenses for the specific language governing
-	permissions and limitations under the Licenses.
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
+    
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
 */
 using System;
-namespace MCGalaxy.Commands
-{
-    public sealed class CmdRoll : Command
-    {
+namespace MCGalaxy.Commands {
+    
+    public sealed class CmdRoll : Command {
+        
         public override string name { get { return "roll"; } }
         public override string shortcut { get { return ""; } }
         public override string type { get { return CommandTypes.Other; } }
@@ -27,18 +27,19 @@ namespace MCGalaxy.Commands
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public CmdRoll() { }
 
-        public override void Use(Player p, string message)
-        {
-            int min, max; Random rand = new Random();
-            try { min = int.Parse(message.Split(' ')[0]); }
-            catch { min = 1; }
-            try { max = int.Parse(message.Split(' ')[1]); }
-            catch { max = 7; }
+        public override void Use(Player p, string message) {
+            string[] args = message.Split(' ');
+            Random rand = new Random();
+            int min, max;
+            
+            if (!int.TryParse(args[0], out min)) min = 1;
+            if (args.Length == 1 || !int.TryParse(args[1], out max)) max = 7;
+            if (min > max) { int a = min; min = max; max = a; }
 
-            Player.GlobalMessage(p.color + p.name + Server.DefaultColor + " rolled a &a" + rand.Next(Math.Min(min, max), Math.Max(min, max) + 1).ToString() + Server.DefaultColor + " (" + Math.Min(min, max) + "|" + Math.Max(min, max) + ")");
+            Player.GlobalMessage(p.color + p.name + " %Srolled a &a" + rand.Next(min, max + 1) + " %S(" + min + "|" + max + ")");
         }
-        public override void Help(Player p)
-        {
+        
+        public override void Help(Player p) {
             Player.SendMessage(p, "/roll [min] [max] - Rolls a random number between [min] and [max].");
         }
     }
