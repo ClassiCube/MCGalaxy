f4491f66e0df3372b6dc8c969c1a56031f002418
UnknownShadow200 <unknownshadow200@gmail.com>
2016-03-18 09:18:28 +1100

    Modularise /missile and /gun commands. Add /missile tp.

---
+++ Commands/Fun/CmdMissile.cs
@@ -0,0 +1,149 @@
+/*
+    Copyright 2011 MCForge
+        
+    Dual-licensed under the    Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
+ */
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using MCGalaxy.Drawing.Ops;
+
+namespace MCGalaxy.Commands {
+    
+    public sealed class CmdMissile : WeaponCmd {
+
+        public override string name { get { return "missile"; } }
+        public override string shortcut { get { return ""; } }
+        protected override string Weapon { get { return "Missile"; } }
+
+        protected override void Blockchange1(Player p, ushort x, ushort y, ushort z, byte type, byte extType) {
+            if (!p.staticCommands) {
+                p.ClearBlockchange();
+                p.aiming = false;
+            }
+            p.RevertBlock(x, y, z);           
+
+            if (p.modeType != Block.air)
+                type = p.modeType;
+
+            Thread gunThread = new Thread(() => DoShoot(p, type, extType));
+            gunThread.Name = "MCG_Missile";
+            gunThread.Start();
+        }
+
+        void DoShoot(Player p, byte type, byte extType) {
+            CatchPos bp = (CatchPos)p.blockchangeObject;
+            List<Pos> previous = new List<Pos>(), allBlocks = new List<Pos>();
+            Pos pos = MakePos(p);
+
+            int total = 0;
+            List<FillPos> buffer = new List<FillPos>(2);
+            while (true) {
+                Pos start = MakePos(p);
+                total++;
+                double a = Math.Sin(((double)(128 - p.rot[0]) / 256) * 2 * Math.PI);
+                double b = Math.Cos(((double)(128 - p.rot[0]) / 256) * 2 * Math.PI);
+                double c = Math.Cos(((double)(p.rot[1] + 64) / 256) * 2 * Math.PI);
+
+                Pos lookedAt;
+                int i;
+                for (i = 1; ; i++) {
+                    lookedAt.x = (ushort)Math.Round(start.x + (double)(a * i));
+                    lookedAt.y = (ushort)Math.Round(start.y + (double)(c * i));
+                    lookedAt.z = (ushort)Math.Round(start.z + (double)(b * i));
+
+                    byte tile = p.level.GetTile(lookedAt.x, lookedAt.y, lookedAt.z);
+                    if (tile == Block.Zero) break;
+
+                    if (tile != Block.air && !allBlocks.Contains(lookedAt) && HandlesHitBlock(p, tile, bp, pos, false))
+                        break;
+
+                    Player hit = GetPlayer(p, lookedAt, true);
+                    if (hit != null) {
+                        lookedAt = MakePos(hit); break;
+                    }
+                }
+
+                lookedAt.x = (ushort)Math.Round(start.x + (double)(a * (i - 1)));
+                lookedAt.y = (ushort)Math.Round(start.y + (double)(c * (i - 1)));
+                lookedAt.z = (ushort)Math.Round(start.z + (double)(b * (i - 1)));
+                FindNext(lookedAt, ref pos, buffer);
+
+                byte by = p.level.GetTile(pos.x, pos.y, pos.z);
+                if (total > 3) {
+                    if (by != Block.air && !allBlocks.Contains(pos) && HandlesHitBlock(p, by, bp, pos, true))
+                        break;
+
+                    p.level.Blockchange(pos.x, pos.y, pos.z, type, extType);
+                    previous.Add(pos);
+                    allBlocks.Add(pos);
+
+                    Player hitP = GetPlayer(p, pos, true);
+                    if (hitP != null) {
+                        if (p.level.physics >= 3 && bp.ending >= EndType.Explode)
+                            hitP.HandleDeath(Block.stone, " was blown up by " + p.color + p.name, true);
+                        else
+                            hitP.HandleDeath(Block.stone, " was hit a missile from " + p.color + p.name);
+                        break;
+                    }
+
+                    if (pos.x == lookedAt.x && pos.y == lookedAt.y && pos.z == lookedAt.z) {
+                        if (p.level.physics >= 3 && bp.ending >= EndType.Explode && p.allowTnt) {
+                            p.level.MakeExplosion(lookedAt.x, lookedAt.y, lookedAt.z, 2);
+                            break;
+                        }
+                    }
+
+                    if (previous.Count > 12) {
+                        p.level.Blockchange(previous[0].x, previous[0].y, previous[0].z, Block.air);
+                        previous.RemoveAt(0);
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+
+            if (bp.ending == EndType.Teleport) {
+                int index = previous.Count - 3;
+                if (index >= 0 && index < previous.Count)
+                    DoTeleport(p, previous[index]);
+            }
+            foreach (Pos pos1 in previous) {
+                p.level.Blockchange(pos1.x, pos1.y, pos1.z, Block.air);
+                Thread.Sleep(100);
+            }
+        }
+        
+        static Pos MakePos(Player p) {
+            Pos pos;
+            pos.x = (ushort)(p.pos[0] / 32);
+            pos.y = (ushort)(p.pos[1] / 32);
+            pos.z = (ushort)(p.pos[2] / 32);
+            return pos;
+        }
+        
+        void FindNext(Pos lookedAt, ref Pos pos, List<FillPos> buffer) {
+            LineDrawOp.DrawLine(pos.x, pos.y, pos.z, 2, lookedAt.x, lookedAt.y, lookedAt.z, buffer);
+            FillPos end = buffer[buffer.Count - 1];
+            pos.x = end.X; pos.y = end.Y; pos.z = end.Z;
+            buffer.Clear();
+        }
+        
+        public override void Help(Player p) {
+            Player.SendMessage(p, "/missile [at end] - Allows you to fire missiles at people");
+            Player.SendMessage(p, "Available [at end] values: &cexplode, destroy, tp");
+            Player.SendMessage(p, "Differs from /gun in that the missile is guided");
+        }
+    }
+}
---
+++ Commands/Fun/WeaponCmd.cs
@@ -0,0 +1,180 @@
+﻿/*
+    Copyright 2011 MCForge
+    
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
+ */
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MCGalaxy.Commands {
+    
+    public abstract class WeaponCmd : Command {
+        
+        public override string type { get { return CommandTypes.Other; } }
+        public override bool museumUsable { get { return false; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.AdvBuilder; } }
+        protected abstract string Weapon { get; }
+        
+        public override void Use(Player p, string message) {
+            if (!p.level.guns) {
+                Player.SendMessage(p, Weapon + "s cannot be used on this map!"); return;
+            }
+            if (p.hasflag != null) {
+                Player.SendMessage(p, "You can't use a " + Weapon.ToLower() + " while you have the flag!"); return;
+            }
+
+            if (p.aiming && message == "") {
+                p.aiming = false;
+                p.ClearBlockchange();
+                Player.SendMessage(p, "Disabled " + Weapon.ToLower() );
+                return;
+            }
+
+            CatchPos cpos = default(CatchPos);
+            cpos.ending = GetEnd(p, message);
+            if (cpos.ending == EndType.Invalid) return;
+            p.blockchangeObject = cpos;
+            p.ClearBlockchange();
+            p.Blockchange += new Player.BlockchangeEventHandler(Blockchange1);
+
+            p.SendMessage(Weapon + " mode engaged, fire at will");
+            if (p.aiming) return;
+
+            p.aiming = true;
+            Thread aimThread = new Thread(() => DoAim(p));
+            aimThread.Name = "MCG_WeaponAim";
+            aimThread.Start();
+        }
+        
+        EndType GetEnd(Player p, string mode) {
+            if (mode == "") return EndType.Normal;
+            if (mode.CaselessEquals("destroy")) return EndType.Destroy;
+            if (mode.CaselessEquals("tp") || mode.CaselessEquals("teleport")) return EndType.Teleport;
+            
+            if (mode.CaselessEquals("explode")) {
+                if (!p.allowTnt) Player.SendMessage(p, "Tnt usage is currently disallowed, switching to normal gun.");
+                return p.allowTnt ? EndType.Explode : EndType.Destroy;
+            }
+            if (mode.CaselessEquals("laser")) {
+                if (!p.allowTnt) Player.SendMessage(p, "Tnt usage is currently disallowed, switching to normal gun.");
+                return p.allowTnt ? EndType.Laser : EndType.Destroy;
+            }
+            Help(p);
+            return EndType.Invalid;
+        }
+        
+        void DoAim(Player p) {
+            Pos pos;
+            List<Pos> lastSent = new List<Pos>(), toSend = new List<Pos>();
+            while (p.aiming) {
+                double a = Math.Sin(((double)(128 - p.rot[0]) / 256) * 2 * Math.PI);
+                double b = Math.Cos(((double)(128 - p.rot[0]) / 256) * 2 * Math.PI);
+                double c = Math.Cos(((double)(p.rot[1] + 64) / 256) * 2 * Math.PI);
+
+                try {
+                    ushort x = (ushort)Math.Round((ushort)(p.pos[0] / 32) + a * 3);
+                    ushort y = (ushort)Math.Round((ushort)(p.pos[1] / 32) + c * 3);
+                    ushort z = (ushort)Math.Round((ushort)(p.pos[2] / 32) + b * 3);
+
+                    if (x >= p.level.Width || y >= p.level.Height || z >= p.level.Length) {
+                        Thread.Sleep(20); continue;
+                    }
+
+                    for (ushort xx = x; xx <= x + 1; xx++)
+                        for (ushort yy = (ushort)(y - 1); yy <= y; yy++)
+                            for (ushort zz = z; zz <= z + 1; zz++)
+                    {
+                        if (p.level.GetTile(xx, yy, zz) == Block.air) {
+                            pos.x = xx; pos.y = yy; pos.z = zz;
+                            toSend.Add(pos);
+                        }
+                    }
+
+                    // Revert all glass blocks now not in the ray from the player's direction
+                    for (int i = 0; i < lastSent.Count; i++) {
+                        Pos cP = lastSent[i];
+                        if (toSend.Contains(cP)) continue;
+                        
+                        p.SendBlockchange(cP.x, cP.y, cP.z, Block.air);
+                        lastSent.RemoveAt(i); i--;
+                    }
+
+                    // Place the new glass blocks that are in the ray from the player's direction
+                    foreach (Pos cP in toSend) {
+                        if (lastSent.Contains(cP)) continue;
+                        lastSent.Add(cP);
+                        p.SendBlockchange(cP.x, cP.y, cP.z, Block.glass);
+                    }
+                    toSend.Clear();
+                }
+                catch { }
+                Thread.Sleep(20);
+            }
+            
+            foreach (Pos cP in lastSent)
+                p.SendBlockchange(cP.x, cP.y, cP.z, Block.air);
+        }
+        
+        protected abstract void Blockchange1(Player p, ushort x, ushort y, ushort z, byte type, byte extType);
+        
+        protected Player GetPlayer(Player p, Pos pos, bool skipSelf) {
+            Player[] players = PlayerInfo.Online.Items;
+            foreach (Player pl in players) {
+                if (pl.level != p.level) continue;
+                if (p == pl && skipSelf) continue;
+                
+                if (Math.Abs((pl.pos[0] / 32) - pos.x) <= 1
+                    && Math.Abs((pl.pos[1] / 32) - pos.y) <= 1
+                    && Math.Abs((pl.pos[2] / 32) - pos.z) <= 1)
+                {
+                    return pl;
+                }
+            }
+            return null;
+        }
+        
+        protected static void DoTeleport(Player p, Pos pos) {
+            try {
+                p.SendPos(0xFF, (ushort)(pos.x * 32), (ushort)(pos.y * 32 + 32), (ushort)(pos.z * 32), p.rot[0], p.rot[1]);
+            } catch {
+            }
+        }
+        
+        protected bool HandlesHitBlock(Player p, byte type, CatchPos bp, Pos pos, bool doExplode) {
+            if (p.level.physics < 2 || bp.ending == EndType.Teleport
+                || bp.ending == EndType.Normal) return true;
+            
+            if (bp.ending == EndType.Destroy) {
+                if ((!Block.FireKill(type) && !Block.NeedRestart(type)) && type != Block.glass) {
+                    return true;
+                }
+            } else if (p.level.physics >= 3) {
+                if (type != Block.glass && p.allowTnt) {
+                    if (doExplode)
+                        p.level.MakeExplosion(pos.x, pos.y, pos.z, 1);
+                    return true;
+                }
+            } else {
+                return true;
+            }
+            return false;
+        }
+
+        protected struct Pos { public ushort x, y, z; }
+        protected struct CatchPos { public ushort x, y, z; public EndType ending; }
+        protected enum EndType { Invalid, Normal, Destroy, Teleport, Explode, Laser };
+    }
+}
