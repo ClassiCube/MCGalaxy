04f6606ee8cdbc4d296eec724ca151aa822f7a56
UnknownShadow200 <unknownshadow200@gmail.com>
2016-04-14 17:19:01 +1000

    Cleanup /awardmod.

--- Commands/Economy/CmdAwardMod.cs
+++ Commands/Economy/CmdAwardMod.cs
@@ -1,19 +1,19 @@
 /*
-	Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-	
-	Dual-licensed under the	Educational Community License, Version 2.0 and
-	the GNU General Public License, Version 3 (the "Licenses"); you may
-	not use this file except in compliance with the Licenses. You may
-	obtain a copy of the Licenses at
-	
-	http://www.opensource.org/licenses/ecl2.php
-	http://www.gnu.org/licenses/gpl-3.0.html
-	
-	Unless required by applicable law or agreed to in writing,
-	software distributed under the Licenses are distributed on an "AS IS"
-	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-	or implied. See the Licenses for the specific language governing
-	permissions and limitations under the Licenses.
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
+    
+    Dual-licensed under the    Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
 */
 namespace MCGalaxy.Commands
 {
@@ -25,48 +25,37 @@ namespace MCGalaxy.Commands
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Admin; } }
         public CmdAwardMod() { }
+        static char[] trimChars = { ' ' }, awardArgs = { ':' };
 
-        public override void Use(Player p, string message)
-        {
-            if (message == "" || message.IndexOf(' ') == -1) { Help(p); return; }
+        public override void Use(Player p, string message) {
+            if (message == "" || message.IndexOf(' ') == -1) { Help(p); return; }            
+            string[] args = message.Split(trimChars, 2);
+            if (!(args[0].CaselessEq("add") || args[0].CaselessEq("del"))) { Help(p); return; }
 
-            bool add = true;
-            if (message.Split(' ')[0].ToLower() == "add")
-            {
-                message = message.Substring(message.IndexOf(' ') + 1);
-            }
-            else if (message.Split(' ')[0].ToLower() == "del")
-            {
-                add = false;
-                message = message.Substring(message.IndexOf(' ') + 1);
-            }
+            if (args[0].CaselessEq("add")) {
+                args = args[1].Split(awardArgs, 2);
+                if (args.Length == 1) { 
+                    Player.SendMessage(p, "&cUse a : to separate the award name from its description."); 
+                    Help(p); return; 
+                }
 
-            if (add)
-            {
-                if (message.IndexOf(":") == -1) { Player.SendMessage(p, "&cMissing a colon!"); Help(p); return; }
-                string awardName = message.Split(':')[0].Trim();
-                string description = message.Split(':')[1].Trim();
-
-                if (!Awards.AddAward(awardName, description))
-                    Player.SendMessage(p, "This award already exists!");
+                if (!Awards.AddAward(args[0], args[1]))
+                    Player.SendMessage(p, "This award already exists.");
                 else
-                    Player.GlobalMessage("Award added: &6" + awardName + " : " + description);
-            }
-            else
-            {
-                if (!Awards.RemoveAward(message))
-                    Player.SendMessage(p, "This award doesn't exist!");
+                    Player.GlobalMessage("Award added: &6" + args[0] + " : " + args[1]);
+            } else {
+                if (!Awards.RemoveAward(args[1]))
+                    Player.SendMessage(p, "This award doe not exist.");
                 else
-                    Player.GlobalMessage("Award removed: &6" + message);
+                    Player.GlobalMessage("Award removed: &6" + args[1]);
             }
-
             Awards.Save();
         }
-        public override void Help(Player p)
-        {
-            Player.SendMessage(p, "/awardmod <add/del> [award name] : [description]");
-            Player.SendMessage(p, "Adds or deletes a reward with the name [award name]");
-            Player.SendMessage(p, "&b/awardmod add Bomb joy : Bomb lots of people" + Server.DefaultColor + " is an example");
+        
+        public override void Help(Player p) {
+            Player.SendMessage(p, "%T/awardmod <add/del> [award name] : [description]");
+            Player.SendMessage(p, "%HAdds or deletes a reward with the name [award name]");
+            Player.SendMessage(p, "%T/awardmod add Bomb joy : Bomb lots of people %His an example");
         }
     }
 }
