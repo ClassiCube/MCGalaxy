6803f6054be8c9f240c2042875f6239c96fb6735
UnknownShadow200 <unknownshadow200@gmail.com>
2016-04-14 17:29:13 +1000

    Cleanup /award.

--- Commands/Economy/CmdAward.cs
+++ Commands/Economy/CmdAward.cs
@@ -1,85 +1,63 @@
 /*
-	Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
-	
-	Dual-licensed under the	Educational Community License, Version 2.0 and
-	the GNU General Public License, Version 3 (the "Licenses"); you may
-	not use this file except in compliance with the Licenses. You may
-	obtain a copy of the Licenses at
-	
-	http://www.opensource.org/licenses/ecl2.php
-	http://www.gnu.org/licenses/gpl-3.0.html
-	
-	Unless required by applicable law or agreed to in writing,
-	software distributed under the Licenses are distributed on an "AS IS"
-	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-	or implied. See the Licenses for the specific language governing
-	permissions and limitations under the Licenses.
+    Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
+    
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
 */
-namespace MCGalaxy.Commands
-{
-    public sealed class CmdAward : Command
-    {
+namespace MCGalaxy.Commands {
+    
+    public sealed class CmdAward : Command {
+        
         public override string name { get { return "award"; } }
         public override string shortcut { get { return ""; } }
         public override string type { get { return CommandTypes.Economy; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public CmdAward() { }
-
-        public override void Use(Player p, string message)
-        {
+        static char[] trimChars = { ' ' };
+        
+        public override void Use(Player p, string message) {
             if (message == "" || message.IndexOf(' ') == -1) { Help(p); return; }
-
-            bool give = true;
-            if (message.Split(' ')[0].ToLower() == "give")
-            {
-                give = true;
-                message = message.Substring(message.IndexOf(' ') + 1);
-            }
-            else if (message.Split(' ')[0].ToLower() == "take")
-            {
-                give = false;
-                message = message.Substring(message.IndexOf(' ') + 1);
-            }
+            string[] args = message.Split(trimChars, 3);           
+            string pl = args[args.Length - 2];
+            Player who = PlayerInfo.Find(pl);
+            if (who != null) pl = who.name;
             
-            string foundPlayer = message.Split(' ')[0];
-            Player who = PlayerInfo.Find(message);
-            if (who != null) foundPlayer = who.name;
-            string awardName = message.Substring(message.IndexOf(' ') + 1);
-            if (!Awards.ExistsAward(awardName))
-            {
+            string award = args[args.Length - 1];
+            if (!Awards.ExistsAward(award)) {
                 Player.SendMessage(p, "The award you entered doesn't exist");
                 Player.SendMessage(p, "Use /awards for a list of awards");
                 return;
             }
 
-            if (give)
-            {
-                if (Awards.GiveAward(foundPlayer, awardName))
-                {
-                    Player.GlobalMessage(Server.FindColor(foundPlayer) + foundPlayer + " %Swas awarded: &b" + awardName);
+            if (args.Length == 2 || !args[0].CaselessEq("take")) {
+                if (Awards.GiveAward(pl, award)) {
+                    Player.GlobalMessage(Server.FindColor(pl) + pl + " %Swas awarded: &b" + award);
+                } else {
+                    Player.SendMessage(p, "The player already has that award."); return;
                 }
-                else
-                {
-                    Player.SendMessage(p, "The player already has that award!");
-                }
-            }
-            else
-            {
-                if (Awards.TakeAward(foundPlayer, awardName))
-                {
-                    Player.GlobalMessage(Server.FindColor(foundPlayer) + foundPlayer + " %Shad their &b" + awardName + " %Saward removed");
-                }
-                else
-                {
-                    Player.SendMessage(p, "The player didn't have the award you tried to take");
+            } else {
+                if (Awards.TakeAward(pl, award)) {
+                    Player.GlobalMessage(Server.FindColor(pl) + pl + " %Shad their &b" + award + " %Saward removed");
+                } else {
+                    Player.SendMessage(p, "The player didn't have the award you tried to take"); return;
                 }
             }
-
             Awards.Save();
         }
-        public override void Help(Player p)
-        {
+        
+        public override void Help(Player p) {
             Player.SendMessage(p, "/award <give/take> [player] [award] - Gives [player] the [award]");
             Player.SendMessage(p, "If no Give or Take is given, Give is used");
             Player.SendMessage(p, "[award] needs to be the full award's name. Not partial");
--- Commands/Economy/CmdAwardMod.cs
+++ Commands/Economy/CmdAwardMod.cs
@@ -1,7 +1,7 @@
 /*
     Copyright 2010 MCSharp team (Modified for use with MCZall/MCLawl/MCGalaxy)
     
-    Dual-licensed under the    Educational Community License, Version 2.0 and
+    Dual-licensed under the Educational Community License, Version 2.0 and
     the GNU General Public License, Version 3 (the "Licenses"); you may
     not use this file except in compliance with the Licenses. You may
     obtain a copy of the Licenses at
@@ -15,10 +15,10 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
 */
-namespace MCGalaxy.Commands
-{
-    public sealed class CmdAwardMod : Command
-    {
+namespace MCGalaxy.Commands {
+	
+    public sealed class CmdAwardMod : Command {
+		
         public override string name { get { return "awardmod"; } }
         public override string shortcut { get { return ""; } }
         public override string type { get { return CommandTypes.Economy; } }
@@ -39,15 +39,17 @@ namespace MCGalaxy.Commands
                     Help(p); return; 
                 }
 
-                if (!Awards.AddAward(args[0], args[1]))
-                    Player.SendMessage(p, "This award already exists.");
-                else
+                if (!Awards.AddAward(args[0], args[1])) {
+                    Player.SendMessage(p, "This award already exists."); return;
+                } else {
                     Player.GlobalMessage("Award added: &6" + args[0] + " : " + args[1]);
+                }
             } else {
-                if (!Awards.RemoveAward(args[1]))
-                    Player.SendMessage(p, "This award doe not exist.");
-                else
+                if (!Awards.RemoveAward(args[1])) {
+                    Player.SendMessage(p, "This award does not exist."); return;
+                } else {
                     Player.GlobalMessage("Award removed: &6" + args[1]);
+                }
             }
             Awards.Save();
         }
