4db3707e61137e5a663687d240d3bdbd5432caba
UnknownShadow200 <unknownshadow200@gmail.com>
2016-04-24 10:19:41 +1000

    Remove /update help and make /update less awful. (Thanks WhatIDoHere)

--- Commands/Information/CmdUpdate.cs
+++ Commands/Information/CmdUpdate.cs
@@ -23,29 +23,20 @@ namespace MCGalaxy.Commands
         public override string shortcut { get { return ""; } }
         public override string type { get { return CommandTypes.Information; } }
         public override bool museumUsable { get { return true; } }
-        public override LevelPermission defaultRank { get { return LevelPermission.AdvBuilder; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
         public CmdUpdate() { }
 
-        public override void Use(Player p, string message)
-        {
-            if (message.ToLower() != "force" && message.ToLower() != "help")
-            {
+        public override void Use(Player p, string message) {
+            if (message.ToLower() != "force") {
                 if (p == null || p.group.Permission > defaultRank) MCGalaxy.Gui.App.UpdateCheck(false, p);
                 else Player.SendMessage(p, "Ask an " + Group.findPerm(defaultRank).name + "+ to do it!");
-            }
-            else if (message.ToLower() == "help")
-            {
-                Help(p);
-                return;
-            }
-            else
-            {
+            } else {
                 if (p == null || p.group.Permission > defaultRank) MCGalaxy.Gui.App.PerformUpdate();
                 else Player.SendMessage(p, "Ask an " + Group.findPerm(defaultRank).name + "+ to do it!");
             }
         }
-        public override void Help(Player p)
-        {
+        
+        public override void Help(Player p) {
             Player.SendMessage(p, "/update - Updates the server if it's out of date");
             Player.SendMessage(p, "/update force - Forces the server to update");
         }
