9205b4faf59f6199323e1c01b24a21dfae085d71
UnknownShadow200 <unknownshadow200@gmail.com>
2016-05-28 14:42:27 +1000

    Remove /serverreport, integrate it into /serverinfo.

--- Commands/Information/CmdServerInfo.cs
+++ Commands/Information/CmdServerInfo.cs
@@ -16,10 +16,12 @@
     permissions and limitations under the Licenses.
  */
 using System;
+using System.Diagnostics;
+
 namespace MCGalaxy.Commands {
     public sealed class CmdServerInfo : Command {
-        public override string name { get { return "sinfo"; } }
-        public override string shortcut { get { return ""; } }
+        public override string name { get { return "serverinfo"; } }
+        public override string shortcut { get { return "sinfo"; } }
         public override string type { get { return CommandTypes.Information; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
@@ -27,7 +29,10 @@ namespace MCGalaxy.Commands {
         public override CommandAlias[] Aliases {
             get { return new[] { new CommandAlias("host"), new CommandAlias("zall") }; }
         }
-
+        public override CommandPerm[] AdditionalPerms {
+            get { return new[] { new CommandPerm(LevelPermission.Admin, "+ can see server CPU and memory usage") }; }
+        }
+        
         public override void Use(Player p, string message) {
             if (message != "") { Help(p); return; }
             
@@ -38,8 +43,8 @@ namespace MCGalaxy.Commands {
                            "Currency is &3" + Server.moneys + "%S.");
             
             TimeSpan up = DateTime.UtcNow - Server.StartTime;
-            Player.Message(p, "Been online for &b" + WhoInfo.Shorten(up, true) +
-                           "%S, and is runing &bMCGalaxy &a" + Server.VersionString +
+            Player.Message(p, "Been up for &b" + WhoInfo.Shorten(up, true) +
+                           "%S, and is running &bMCGalaxy &a" + Server.VersionString +
                            "%S (based on &bMCForge %Sand &bMCLawl%S).");
             Command.all.Find("devs").Use(p, "");
 
@@ -50,6 +55,30 @@ namespace MCGalaxy.Commands {
                 Player.Message(p, "Owner is &3" + owner + ". %SConsole state: &3" + Server.ZallState);
             else
                 Player.Message(p, "Console state: &3" + Server.ZallState);
+            
+            if (CheckAdditionalPerm(p))
+                ShowServerStats(p);
+        }
+        
+        void ShowServerStats(Player p) {
+            Process proc = Process.GetCurrentProcess();
+            if (Server.PCCounter == null) {
+                Player.Message(p, "Starting performance counters...one second");
+                Server.PCCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                Server.PCCounter.BeginInit();
+                Server.PCCounter.NextValue();
+
+                Server.ProcessCounter = new PerformanceCounter("Process", "% Processor Time", proc.ProcessName);
+                Server.ProcessCounter.BeginInit();
+                Server.ProcessCounter.NextValue();
+            }
+            
+            // Private Bytes because it is what the process has reserved for itself
+            int threads = proc.Threads.Count;
+            int mem = (int)Math.Round(proc.PrivateMemorySize64 / 1048576.0);
+            double cpu = Server.ProcessCounter.NextValue(), all = Server.PCCounter.NextValue();
+            Player.Message(p, "&a{0}% %SCPU usage, &a{1}% %Sby all processes", cpu.ToString("F2"), all.ToString("F2"));
+            Player.Message(p, "&a{0}%S threads, using &a{1}%S megabytes of memory", threads, mem);
         }
         
         public override void Help(Player p) {
