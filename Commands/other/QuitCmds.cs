ca7902edac5b6b665994347ae65293103f30261d
UnknownShadow200 <unknownshadow200@gmail.com>
2016-06-05 15:36:20 +1000

    Combine various quit-type commands into one class.

---
+++ Commands/other/QuitCmds.cs
@@ -0,0 +1,95 @@
+﻿/*
+    Copyright 2010 MCLawl Team - Written by Valek (Modified for use with MCGalaxy)
+    Copyright 2011 MCForge
+        
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
+*/
+using System;
+using System.Threading;
+
+namespace MCGalaxy.Commands {    
+    public sealed class CmdRagequit : Command {        
+        public override string name { get { return "ragequit"; } }
+        public override string shortcut { get { return "rq"; } }
+        public override string type { get { return CommandTypes.Other; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
+        
+        public override void Use(Player p, string message)  {
+            p.LeaveServer("RAGEQUIT!!", "RAGEQUIT!!");
+        }
+
+        public override void Help(Player p) {
+            Player.Message(p, "/ragequit - Makes you ragequit");
+        }
+    }
+    
+    public sealed class CmdQuit : Command {        
+        public override string name { get { return "quit"; } }
+        public override string shortcut { get { return ""; } }
+        public override string type { get { return CommandTypes.Other; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+
+        public override void Use(Player p, string message) {
+            string msg = message != "" ? "Left the game: " + message : "Left the game.";
+            p.LeaveServer(msg, msg);
+        }
+        
+        public override void Help(Player p) {
+            Player.Message(p, "/quit <reason> - Leave the server.");
+        }
+    }
+    
+    public sealed class CmdCrashServer : Command {        
+        public override string name { get { return "crashserver"; } }
+        public override string shortcut { get { return "crash"; } }
+        public override string type { get { return CommandTypes.Moderation; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
+
+        public override void Use(Player p, string message) {
+            if (message != "") { Help(p); return; }
+            int code = p.random.Next(int.MinValue, int.MaxValue);
+            string msg = "Server crash! Error code 0x" + Convert.ToString(code, 16).ToUpper();
+            p.LeaveServer(msg, msg);
+        }
+        
+        public override void Help(Player p) {
+            Player.Message(p, "/crashserver - Crash the server with a generic error");
+        }
+    }
+    
+    public sealed class CmdHacks : Command {
+        public override string name { get { return "hacks"; } }
+        public override string shortcut { get { return "hax"; } }
+        public override string type { get { return CommandTypes.Moderation; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
+
+        public override void Use(Player p, string message) {
+            if (message != "") {
+                Player.Message(p, "&cIncorrect syntax. Abuse detected.");
+                Thread.Sleep(3000);
+            }
+            const string msg = "Your IP has been backtraced + reported to FBI Cyber Crimes Unit.";
+            p.LeaveServer(msg, msg);
+        }
+        
+        public override void Help(Player p) {
+            Player.Message(p, "/hacks - Performs various server hacks. OPERATORS ONLY!!!");
+        }
+    }
+}
