a3ee120e618d39daed5dc68c742e646acc20d666
UnknownShadow200 <unknownshadow200@gmail.com>
2016-08-04 12:35:09 +1000

    Combine simple brushes code into one files.

--- Drawing/Brushes/SimpleBrushes.cs
+++ Drawing/Brushes/SimpleBrushes.cs
@@ -16,11 +16,25 @@
     permissions and limitations under the Licenses.
  */
 using System;
-using System.Collections.Generic;
 using MCGalaxy.Commands.Building;
 using MCGalaxy.Drawing.Ops;
 
-namespace MCGalaxy.Drawing.Brushes {  
+namespace MCGalaxy.Drawing.Brushes {    
+    public sealed class SolidBrush : Brush {
+        readonly byte block, extBlock;
+        
+        public SolidBrush(byte type, byte extType) {
+            this.block = type;
+            this.extBlock = extType;
+        }
+        
+        public override string Name { get { return "Normal"; } }
+        
+        public override byte NextBlock(DrawOp op) { return block; }
+        
+        public override byte NextExtBlock(DrawOp op) { return extBlock; }
+    }
+	
     public sealed class StripedBrush : Brush {
         readonly byte block1, extBlock1, block2, extBlock2;
         
@@ -37,6 +51,25 @@ namespace MCGalaxy.Drawing.Brushes {
         
         public override byte NextExtBlock(DrawOp op) {
             return ((op.Coords.X + op.Coords.Y + op.Coords.Z) & 3) <= 1 ? extBlock1 : extBlock2;
+        }
+    }
+	
+    public sealed class CheckeredBrush : Brush {
+        readonly byte block1, extBlock1, block2, extBlock2;
+        
+        public CheckeredBrush(byte block1, byte extBlock1, byte block2, byte extBlock2) {
+            this.block1 = block1; this.extBlock1 = extBlock1;
+            this.block2 = block2; this.extBlock2 = extBlock2;
+        }
+        
+        public override string Name { get { return "Checkered"; } }
+        
+        public override byte NextBlock(DrawOp op) {
+            return ((op.Coords.X + op.Coords.Y + op.Coords.Z) & 1) == 0 ? block1 : block2;
+        }
+        
+        public override byte NextExtBlock(DrawOp op) {
+            return ((op.Coords.X + op.Coords.Y + op.Coords.Z) & 1) == 0 ? extBlock1 : extBlock2;
         }
     }
 }
