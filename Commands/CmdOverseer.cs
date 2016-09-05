329fdb24e9f01c3acc49b9578873fcaf03526c44
UnknownShadow200 <unknownshadow200@gmail.com>
2016-09-06 06:51:16 +1000

    Don't show 'X went to level' for /os go when server has load on goto enabled.

--- Commands/CmdOverseer.cs
+++ Commands/CmdOverseer.cs
@@ -59,7 +59,7 @@ namespace MCGalaxy.Commands {
                 
                 Level[] loaded = LevelInfo.Loaded.Items;
                 if (LevelInfo.FindExact(map) == null)
-                    CmdLoad.LoadLevel(p, map);
+                    CmdLoad.LoadLevel(p, map, Server.AutoLoad);
                 if (LevelInfo.FindExact(map) != null)
                     PlayerActions.ChangeMap(p, map);
             } else if (cmd == "LB" || cmd == "LEVELBLOCK") {
