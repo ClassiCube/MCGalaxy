83e6b1a9ac4f7620b4b2a43290275eaa32986d10
UnknownShadow200 <unknownshadow200@gmail.com>
2017-02-03 15:44:52 +1100

    use thick borders for main gui log box, gets rid of nearly allwinapi specific code

--- MCGalaxy/Commands/building/CmdTree.cs
+++ MCGalaxy/Commands/building/CmdTree.cs
@@ -43,7 +43,7 @@ namespace MCGalaxy.Commands.Building {
             dArgs.tree = tree;
             dArgs.brushMsg = brushMsg;
             dArgs.value = -1;
-            
+
             if (brushMsg != "") {
                 if (!p.group.CanExecute("brush")) {
                     Player.Message(p, "You cannot use %T/brush%S, so therefore cannot use %T/tree%S with a brush."); return;
