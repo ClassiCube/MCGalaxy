644663a0a7cd9f27b23e0d01a8f68f72d27dd0c8
UnknownShadow200 <unknownshadow200@gmail.com>
2017-07-22 18:09:36 +1000

    Rewrite /replacebrush to use DrawCmd too

--- MCGalaxy/Commands/building/CmdReplaceBrush.cs
+++ MCGalaxy/Commands/building/CmdReplaceBrush.cs
@@ -18,72 +18,51 @@
 using System;
 using MCGalaxy.Drawing.Brushes;
 using MCGalaxy.Drawing.Ops;
-using MCGalaxy.Maths;
 
 namespace MCGalaxy.Commands.Building {    
-    public class CmdReplaceBrush : Command {
+    public class CmdReplaceBrush : DrawCmd {
         public override string name { get { return "replacebrush"; } }
         public override string shortcut { get { return "rb"; } }
-        public override string type { get { return CommandTypes.Building; } }
-        public override bool museumUsable { get { return false; } }
         public override LevelPermission defaultRank { get { return LevelPermission.AdvBuilder; } }
-        public override bool SuperUseable { get { return false; } }
+        protected virtual bool ReplaceNot { get { return false; } }
         
-        public override void Use(Player p, string message) {
-            if (message == "") { Help(p); return; }
+        protected override DrawOp GetDrawOp(DrawArgs dArgs) {
+            string[] args = dArgs.Message.SplitSpaces(3);
+            Player p = dArgs.Player;
+            if (args.Length < 2) { Help(p); return null; }
             
             string replaceCmd = ReplaceNot ? "replacenot" : "replace";
             if (!p.group.CanExecute(replaceCmd) || !p.group.CanExecute("brush")) {
                 Player.Message(p, "You cannot use /brush and/or /" + replaceCmd + 
-                                   ", so therefore cannot use this command."); return;
+                                   ", so therefore cannot use this command."); return null;
             }
             
-            message = message.ToLower();
-            string[] parts = message.SplitSpaces(3);
-            if (!ValidateArgs(p, parts)) return;
- 
-            Player.Message(p, "Place or break two blocks to determine the edges.");
-            p.MakeSelection(2, message, DoReplace);
-        }
-        
-        bool ValidateArgs(Player p, string[] args) {
-            if (args.Length < 2) { Help(p); return false; }
             
-            ExtBlock block;
-            if (!CommandParser.GetBlockIfAllowed(p, args[0], out block)) return false;
+            ExtBlock target;
+            if (!CommandParser.GetBlockIfAllowed(p, args[0], out target)) return null;
             
             BrushFactory factory = BrushFactory.Find(args[1]);
             if (factory == null) {
                 Player.Message(p, "No brush found with name \"{0}\".", args[1]);
                 Player.Message(p, "Available brushes: " + BrushFactory.Available);
-                return false;
+                return null;
             }
-            
-            string brushMessage = args.Length > 2 ? args[2] : "";
-            BrushArgs bArgs = new BrushArgs(p, brushMessage, p.GetHeldBlock());
-            return factory.Validate(bArgs);
-        }
-        
-        bool DoReplace(Player p, Vec3S32[] marks, object state, ExtBlock held) {
-            string[] args = ((string)state).SplitSpaces(3);
-            ExtBlock target;
-            if (!CommandParser.GetBlockIfAllowed(p, args[0], out target)) return false;
-            
-            BrushFactory factory = BrushFactory.Find(args[1]);
-            string brushMessage = args.Length > 2 ? args[2] : "";
-            BrushArgs bArgs = new BrushArgs(p, brushMessage, held);
-            Brush brush = factory.Construct(bArgs);
-            if (brush == null) return false;
             
             DrawOp op = null;
             if (ReplaceNot) op = new ReplaceNotDrawOp(target);
             else op = new ReplaceDrawOp(target);
-            
-            DrawOpPerformer.Do(op, brush, p, marks);
-            return true;
+            return op;
         }
         
-        protected virtual bool ReplaceNot { get { return false; } }
+        protected override string GetBrush(DrawArgs dArgs, ref int offset) {
+            string[] args = dArgs.Message.SplitSpaces(3);
+            return args[1];
+        }
+        
+        protected override string GetBrushArgs(DrawArgs dArgs, int usedFromEnd) {
+            string[] args = dArgs.Message.SplitSpaces(3);
+            return args.Length > 2 ? args[2] : "";
+        }
         
         public override void Help(Player p) {
             Player.Message(p, "%T/rb [block] [brush name] <brush args>");
