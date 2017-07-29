44ef5f68b3636c7a114a9570fa7ed14d8cd5fe6f
UnknownShadow200 <unknownshadow200@gmail.com>
2017-07-29 23:21:39 +1000

    Improve output of various parts of /ccols

--- MCGalaxy/Commands/CPE/CmdCustomColors.cs
+++ MCGalaxy/Commands/CPE/CmdCustomColors.cs
@@ -72,7 +72,7 @@ namespace MCGalaxy.Commands.CPE {
             }
             
             char fallback;
-            if (!CheckName(p, args[2]) || !CheckFallback(p, args[3], out fallback)) return;
+            if (!CheckName(p, args[2]) || !CheckFallback(p, args[3], code, out fallback)) return;
             ColorDesc col = default(ColorDesc);
             if (!CommandParser.GetHex(p, args[4], ref col)) return;
             
@@ -84,12 +84,8 @@ namespace MCGalaxy.Commands.CPE {
         void RemoveHandler(Player p, string[] args) {
             if (args.Length < 2) { Help(p); return; }
             
-            char code = args[1][0];
-            if (!Colors.IsDefined(code)) {
-                Player.Message(p, "There is no color with the code {0}.", code);
-                Player.Message(p, "Use \"%T/ccols list\" %Sto see a list of colors.");
-                return;
-            }
+            char code = ParseColor(p, args[1]);
+            if (code == '\0') return;
             
             Colors.Update(Colors.DefaultCol(code));
             Player.Message(p, "Successfully removed a color.");
@@ -105,42 +101,45 @@ namespace MCGalaxy.Commands.CPE {
         
         // Not very elegant, because we don't want the % to be escaped like everywhere else
         internal static string FormatColor(ColorDesc col) {
-            const string format = "{0} &{1}({2})%S - %&S{1}, falls back to &{3}%&{3}{3}";
+            string format = "{0} &{1}({2})%S - %&S{1}, falls back to &{3}%&{3}{3}";
+            if (col.Code == col.Fallback) format = "{0} &{1}({2})%S - %&S{1}";
+
             return String.Format(format, col.Name, col.Code, Utils.Hex(col.R, col.G, col.B), col.Fallback);
         }
         
         void EditHandler(Player p, string[] args) {
             if (args.Length < 4) { Help(p); return; }
             
-            char code = args[1][0];            
-            if (!Colors.IsDefined(code)) {
-                Player.Message(p, "There is no color with the code {0}.", code);
-                Player.Message(p, "Use \"%T/ccols list\" %Sto see a list of colors.");
-                return;
-            }
-            
+            char code = ParseColor(p, args[1]);
+            if (code == '\0') return;            
             ColorDesc col = Colors.List[code];
-            char fallback;
+            
             switch (args[2]) {
                 case "name":
-                    if (!CheckName(p, args[3])) return;
-                    col.Name = args[3]; break;
+                    if (!CheckName(p, args[3])) return;                  
+
+                    Player.Message(p, "Set name of {0} to {1}", col.Name, args[3]);
+                    col.Name = args[3];break;
                 case "fallback":
-                    if (!CheckFallback(p, args[3], out fallback)) return;
+                    char fallback;
+                    if (!CheckFallback(p, args[3], code, out fallback)) return;                    
+                    
+                    Player.Message(p, "Set fallback of {0} to %&S{1}", col.Name, fallback);
                     col.Fallback = fallback; break;
                 case "hex":
                 case "color":
                     ColorDesc rgb = default(ColorDesc);
                     if (!CommandParser.GetHex(p, args[3], ref rgb)) return;
-                    col.R = rgb.R; col.G = rgb.G; col.B = rgb.B;
-                    break;
+                    
+                    Player.Message(p, "Set hex color of {0} to {1}", col.Name, Utils.Hex(rgb.R, rgb.G, rgb.B));
+                    col.R = rgb.R; col.G = rgb.G; col.B = rgb.B; break;
                 default:
                     Help(p); return;
             }
             
             Colors.Update(col);
-            Player.Message(p, "Successfully edited a color.");
         }
+        
         
         static bool CheckName(Player p, string arg) {
             if (Colors.Parse(arg) != "") {
@@ -150,11 +149,31 @@ namespace MCGalaxy.Commands.CPE {
             return true;
         }
         
-        static bool CheckFallback(Player p, string arg, out char fallback) {
+        static char ParseColor(Player p, string arg) {
+            if (arg.Length != 1) {
+                string colCode = Colors.Parse(arg);
+                if (colCode != "") return colCode[1];
+                
+                Player.Message(p, "There is no color \"" + arg + "\".");
+            } else {
+                char code = arg[0];
+                if (Colors.IsDefined(code)) return code;
+                
+                Player.Message(p, "There is no color with the code {0}.", code);
+                Player.Message(p, "Use \"%T/ccols list\" %Sto see a list of colors.");
+            }
+            return '\0';
+        }
+        
+        static bool CheckFallback(Player p, string arg, char code, out char fallback) {
             fallback = arg[0];
             if (!Colors.IsStandard(fallback)) {
                 Player.Message(p, "{0} must be a standard color code.", fallback); return false;
             }
+            if (Colors.IsStandard(code)) {
+                Player.Message(p, "Cannot change fallback of a standard color code."); return false;
+            }
+            
             if (fallback >= 'A' && fallback <= 'F') fallback += ' ';
             return true;
         }
@@ -165,7 +184,7 @@ namespace MCGalaxy.Commands.CPE {
             Player.Message(p, "%H  fallback is the color code shown to non-supporting clients.");
             Player.Message(p, "%T/customcolors remove [code] %H- Removes that custom color.");
             Player.Message(p, "%T/customcolors list [offset] %H- lists all custom colors.");
-            Player.Message(p, "%T/customcolors edit [code] [name/fallback/hex]");
-        }        
+            Player.Message(p, "%T/customcolors edit [code] [name/fallback/hex] [value]");
+        }
     }
 }
