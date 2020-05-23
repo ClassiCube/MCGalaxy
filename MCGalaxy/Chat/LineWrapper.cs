116606ad5f8b70cfe75d6e342ed3db6b1935435a
UnknownShadow200 <unknownshadow200@gmail.com>
2020-05-23 20:16:11 +1000

    fix last commit not compiling

--- MCGalaxy/Chat/LineWrapper.cs
+++ MCGalaxy/Chat/LineWrapper.cs
@@ -14,6 +14,7 @@ permissions and limitations under the Licenses.
 */
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MCGalaxy {
