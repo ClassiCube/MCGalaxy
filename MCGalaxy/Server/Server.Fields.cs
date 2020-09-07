6c7050d0edec4dad07575749dd7b593683089846
UnknownShadow200 <unknownshadow200@gmail.com>
2020-09-07 12:49:00 +1000

    Release 1.9.2.5

--- MCGalaxy/Server/Server.Fields.cs
+++ MCGalaxy/Server/Server.Fields.cs
@@ -40,7 +40,7 @@ namespace MCGalaxy {
         public static PlayerMetaList Notes = new PlayerMetaList("text/notes.txt");
         
         /// <summary> *** DO NOT USE THIS! *** Use VersionString, as this field is a constant and is inlined if used. </summary>
-        public const string InternalVersion = "1.9.2.4";
+        public const string InternalVersion = "1.9.2.5";
         public static string Version { get { return InternalVersion; } }
         
         public static string SoftwareName = "MCGalaxy";
