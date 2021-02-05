b44596dacb93ab748e0235353a4c1cc37965b48f
UnknownShadow200 <unknownshadow200@gmail.com>
2021-02-05 21:07:00 +1100

    Avoid LINQ in /pass, remove DISABLE_OLD_PASSWORDFILE (old hash will be removed from source code eventually)

--- MCGalaxy/Commands/Moderation/CmdPass.cs
+++ MCGalaxy/Commands/Moderation/CmdPass.cs
@@ -21,10 +21,6 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-#if !DISABLE_OLD_PASSWORDFILE
-using System.Collections.Generic;
-using System.Linq;
-#endif
 
 namespace MCGalaxy.Commands.Moderation {
     public sealed class CmdPass : Command2 {
@@ -37,15 +33,15 @@ namespace MCGalaxy.Commands.Moderation {
             get { return new[] { new CommandAlias("SetPass", "set"), new CommandAlias("ResetPass", "reset") }; }
         }
 
+        const string passDir = "extra/passwords/";
         public override void Use(Player p, string message, CommandData data) {
-            if (!Directory.Exists("extra/passwords"))
-                Directory.CreateDirectory("extra/passwords");
             if (data.Rank < Server.Config.VerifyAdminsRank) {
                 Formatter.MessageNeedMinPerm(p, "+ can verify or set a password", Server.Config.VerifyAdminsRank); return;
             }
             
             if (!Server.Config.verifyadmins) { p.Message("Admin verification is not currently enabled."); return; }
             if (message.Length == 0) { Help(p); return; }
+            if (!Directory.Exists(passDir)) Directory.CreateDirectory(passDir);
             
             string[] args = message.SplitSpaces(2);
             if (args.Length == 2 && args[0].CaselessEq("set")) {
@@ -57,48 +53,66 @@ namespace MCGalaxy.Commands.Moderation {
             }
         }
         
-        void VerifyPassword(Player p, string password) {
+        static void OnVerified(Player p) {
+            p.Message("You are now &averified %Sand have &aaccess to admin commands and features.");
+            p.verifiedPass = true;
+            p.Unverified   = false;
+        }
+        
+        static void OnFailed(Player p) {
+            p.passtries++;
+            p.Message("%WWrong Password. %SRemember your password is %Wcase sensitive.");
+            p.Message("Forgot your password? Contact %W{0} %Sto %Wreset it.", Server.Config.OwnerName);
+        }
+        
+        static void StorePassword(string curPath, string name, string pass) {
+            // In case was using .dat password before
+            if (curPath != null) File.Delete(curPath);
+            
+            byte[] hash = ComputeNewHash(name, pass);
+            File.WriteAllBytes(NewHashPath(name), hash);
+        }
+        
+        static void VerifyPassword(Player p, string password) {
             if (!p.Unverified) { p.Message("%WYou are already verified."); return; }
             if (p.passtries >= 3) { p.Kick("Did you really think you could keep on guessing?"); return; }
-
             if (password.IndexOf(' ') >= 0) { p.Message("Your password must be %Wone %Sword!"); return; }
-            if (!HasPassword(p.name)) {
-                p.Message("You have not %Wset a password, %Suse %T/SetPass [Password] %Wto set one!"); return;
+
+            string path = NewHashPath(p.name);
+            // If new format exists, that is always used
+            if (File.Exists(path)) {
+                if (CheckNewHash(path, p.name, password)) {
+                    OnVerified(p);
+                } else {
+                    OnFailed(p);
+                }
+                return;
             }
 
-            if (CheckHash(p.name, password)) {
-                p.Message("You are now &averified %Sand have &aaccess to admin commands and features.");
-                p.verifiedPass = true;
-                p.Unverified   = false;
-
-#if !DISABLE_OLD_PASSWORDFILE
-                string oldpath = HashPath(p.name, false);
-                string path = HashPath(p.name, true);
-                if (oldpath != path) {
-                    File.Delete(oldpath);
-                    byte[] computed = ComputeHash(p.name, password);
-                    File.WriteAllBytes(path, computed);
-                    p.Message("Your password was &areset for the new format");
-                }
-#endif
+            // Fallback onto old format
+            path = FindOldHashPath(p.name);
+            if (path == null) {
+                p.Message("You have not %Wset a password, %Suse %T/SetPass [Password] %Wto set one!");
+            } else if (!CheckOldHash(path, p.name, password)) {
+                OnFailed(p);
             } else {
-                p.passtries++;
-                p.Message("%WWrong Password. %SRemember your password is %Wcase sensitive.");
-                p.Message("Forgot your password? Contact %W{0} %Sto %Wreset it.", Server.Config.OwnerName);
+                OnVerified(p);
+                // Switch password to new format
+                StorePassword(path, p.name, password);
+                p.Message("Your password was &areset for the new format");
             }
         }
         
-        void SetPassword(Player p, string password) {
-            string path = HashPath(p.name);
-            if (p.Unverified && File.Exists(path)) {
+        static void SetPassword(Player p, string password) {
+            string curPath = FindHashPath(p.name);
+            if (p.Unverified && curPath != null) {
                 p.Message("%WYou must first verify with %T/pass [Password] %Wbefore you can change your password.");
                 p.Message("Forgot your password? Contact %W{0} %Sto %Wreset it.", Server.Config.OwnerName);
                 return;
             }
-            if (password.IndexOf(' ') >= 0) { p.Message("%WPassword must be one word."); return; }
             
-            byte[] computed = ComputeHash(p.name, password);
-            File.WriteAllBytes(path, computed);
+            if (password.IndexOf(' ') >= 0) { p.Message("%WPassword must be one word."); return; }
+            StorePassword(curPath, p.name, password);
             p.Message("Your password was &aset to: &c" + password);
         }
         
@@ -114,8 +128,8 @@ namespace MCGalaxy.Commands.Moderation {
                 p.Message("%WOnly console and the server owner may reset passwords."); return;
             }
             
-            string path = HashPath(target.name);
-            if (!File.Exists(path)) {
+            string path = FindHashPath(target.name);
+            if (path == null) {
                 p.Message("{0} %Sdoes not have a password.", p.FormatNick(target));
             } else {
                 File.Delete(path);
@@ -123,8 +137,8 @@ namespace MCGalaxy.Commands.Moderation {
             }
         }
 
-#if !DISABLE_OLD_PASSWORDFILE
-        static byte[] OldComputeHash(string name, string pass) {
+        
+        static byte[] ComputeOldHash(string name, string pass) {
             // Pointless, but kept for backwards compatibility
             pass = pass.Replace("<", "(");
             pass = pass.Replace(">", ")");
@@ -140,87 +154,74 @@ namespace MCGalaxy.Commands.Moderation {
             Array.Copy(dataB, 0, result, nameB.Length, dataB.Length);
             return hash.ComputeHash(result);
         }
-#endif
 
-        static byte[] ComputeHash(string name, string pass) {
+        static byte[] ComputeNewHash(string name, string pass) {
             // The constant string added to the username salt is to mitigate
             // rainbox tables. We should really have a unique salt for each
             // user, but this is close enough.
-            return SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes("0bec662b-416f-450c-8f50-664fd4a41d49" + name.ToLower() + " " + pass));
+            byte[] data = Encoding.UTF8.GetBytes("0bec662b-416f-450c-8f50-664fd4a41d49" + name.ToLower() + " " + pass);
+            return SHA256.Create().ComputeHash(data);
+        }
+        
+        static bool ArraysEqual(byte[] a, byte[] b) {
+            if (a.Length != b.Length) return false;
+            
+            for (int i = 0; i < a.Length; i++) {
+                if (a[i] != b[i]) return false;
+            }
+            return true;
         }
 
-        static bool CheckHash(string name, string pass) {
-            byte[] stored   = File.ReadAllBytes(HashPath(name));
-            byte[] computed = ComputeHash(name, pass);
-
-#if !DISABLE_OLD_PASSWORDFILE
-            // Old style MD5 passwords.
-            byte[] oldcomputed = OldComputeHash(name, pass);
-            if (stored.Length == oldcomputed.Length) {
-                bool oldokay = true;
-                for (int i = 0; i < stored.Length; i++) {
-                    if (stored[i] != oldcomputed[i]) oldokay = false;
-                }
-                if (oldokay) return true;
-            }
+        static bool CheckNewHash(string path, string name, string pass) {
+            byte[] stored   = File.ReadAllBytes(path);
+            byte[] computed = ComputeNewHash(name, pass);
+            return ArraysEqual(computed, stored);
+        }
+        
+        static bool CheckOldHash(string path, string name, string pass) {
+            byte[] stored   = File.ReadAllBytes(path);
+            byte[] computed = ComputeOldHash(name, pass);
 
             // Old passwords stored UTF8 string instead of just the raw 16 byte hashes
             // We need to support both since this behaviour was accidentally changed
             if (stored.Length != computed.Length) {
                 return Encoding.UTF8.GetString(stored) == Encoding.UTF8.GetString(computed);
             }
-#endif
-            for (int i = 0; i < stored.Length; i++) {
-                if (stored[i] != computed[i]) return false;
-            }
-            return true;
+            return ArraysEqual(computed, stored);
+        }
+
+        
+        static string NewHashPath(string name) {
+            return passDir + name.ToLower() + ".pwd";
         }
         
+        static string FindOldHashPath(string name) {
+            string path = passDir + name + ".dat";
+            if (File.Exists(path)) return path;
+
+            // Have to fallback on this for case sensitive file systems
+            foreach (string file in Directory.EnumerateFiles(passDir, ".dat")) {
+                if (file.CaselessEq(path)) return file;
+            }
+            return null;
+        }
+        
+        static string FindHashPath(string name) {
+            string path = NewHashPath(name);
+            if (File.Exists(path)) return path;
+            return FindOldHashPath(name);
+        }
+
+        public static bool HasPassword(string name) { return FindHashPath(name) != null; }
+        
         public override void Help(Player p) {
-            p.Message("%T/Pass reset [Player] %H- Resets the password for that player");
+            p.Message("%T/Pass reset [player] %H- Resets the password for that player");
             p.Message("%H Note: Can only be used by console and the server owner.");
-            p.Message("%T/Pass set [Password] %H- Sets your password to [password]");
+            p.Message("%T/Pass set [password] %H- Sets your password to [password]");
             p.Message("%H Note: %WDo NOT set this as your Minecraft password!");
-            p.Message("%T/Pass [Password]");
+            p.Message("%T/Pass [password]");
             p.Message("%HIf you are an admin, use this command to verify your login.");
             p.Message("%H You will need to be verified to be able to use commands.");
         }
-
-        public static bool HasPassword(string name) { return File.Exists(HashPath(name)); }
-
-#if DISABLE_OLD_PASSWORDFILE
-        static string HashPath(string name) { return "extra/passwords/" + name.ToLower() + ".pwd"; }
-#else
-        static string HashPath(string name, bool ForUpdate = false)
-        {
-            string PassName = "extra/passwords/" + name.ToLower() + ".pwd";
-            if (ForUpdate || File.Exists(PassName))
-                return PassName;
-
-            string OldName = "extra/passwords/" + name + ".dat";
-            if (File.Exists(PassName))
-                return PassName;
-
-            string directory = Path.GetDirectoryName(OldName);
-            IEnumerable<string> foundFiles = EnumerateFilesCI(directory, OldName);
-
-            if (foundFiles.Count() < 1)
-                return PassName;
-
-            return foundFiles.First();
-        }
-
-        private static IEnumerable<string> EnumerateFilesCI(string directory, string pathAndFileName)
-        {
-            foreach (string file in Directory.EnumerateFiles(directory))
-            {
-                if (String.Equals(file, pathAndFileName, StringComparison.OrdinalIgnoreCase) )
-                {
-                    yield return file;
-                }
-            }
-        }
-#endif
-
     }
 }
