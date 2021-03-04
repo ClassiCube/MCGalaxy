52d2271459911e3107612366d1a35d0a415619f4
Derek <derekdinan@users.noreply.github.com>
2021-03-05 06:44:42 +1000

    Use Colors.Strip when unloading plugins
    Some plugins use colour codes to make them stand out in the /plugins list. Prior to this, you could not unload plugins due to the colour code but using Colors.Strip() before finding will fix this.
    
    Fixes ClassiCube/MCGalaxy-Plugins#5 and derekdinan/ClassiCube-Stuff#2

--- MCGalaxy/Commands/Scripting/CmdPlugin.cs
+++ MCGalaxy/Commands/Scripting/CmdPlugin.cs
@@ -83,7 +83,7 @@ namespace MCGalaxy.Commands.Scripting {
         
         static void UnloadPlugin(Player p, string name) {
             int matches;
-            Plugin plugin = Matcher.Find(p, name, out matches, Plugin.all, 
+            Plugin plugin = Matcher.Find(p, Colors.Strip(name), out matches, Plugin.all,
                                          null, pln => pln.name, "plugins");
             if (plugin == null) return;
             
