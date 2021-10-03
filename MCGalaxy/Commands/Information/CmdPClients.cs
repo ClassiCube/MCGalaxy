5fa137e6b3db5f947718293d64660d4ad24b3ea2
forkiesassds <54495659+forkiesassds@users.noreply.github.com>
2021-10-03 16:56:48 +0300

    show preclassic versions in /pclients
    As suggested in https://github.com/UnknownShadow200/MCGalaxy/issues/643#issuecomment-932935591.

--- MCGalaxy/Commands/Information/CmdPClients.cs
+++ MCGalaxy/Commands/Information/CmdPClients.cs
@@ -19,36 +19,62 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MCGalaxy.Commands.Info {  
+namespace MCGalaxy.Commands.Info 
+{  
     public sealed class CmdPClients : Command2 {        
         public override string name { get { return "PClients"; } }
         public override string shortcut { get { return "Clients"; } }
         public override string type { get { return CommandTypes.Information; } }
         public override bool UseableWhenFrozen { get { return true; } }
         
-        public override void Use(Player p, string message, CommandData data) {
+        public override void Use(Player p, string message, CommandData data) 
+        {
             Dictionary<string, List<Player>> clients = new Dictionary<string, List<Player>>();
             Player[] online = PlayerInfo.Online.Items;
             
-            foreach (Player pl in online) {
+            foreach (Player pl in online) 
+            {
                 if (!p.CanSee(pl, data.Rank)) continue;
                 string appName = pl.appName;
-                if (String.IsNullOrEmpty(appName)) appName = "(unknown)";
+                if (String.IsNullOrEmpty(appName)) 
+                { 
+                    switch(pl.ProtocolVersion)
+                    {
+                        case 3:
+                            appName = "(unknown ?-0.0.16a_02 client)";
+                            break;
+                        case 4:
+                            appName = "(unknown 0.0.17a-0.0.18a_02 client)";
+                            break;
+                        case 5:
+                            appName = "(unknown 0.0.19a-0.0.19a_06 client)";
+                            break;
+                        case 6:
+                            appName = "(unknown 0.0.20a-0.0.23a_01 client)";
+                            break;
+                        case 7:
+                            appName = "(unknown 0.28-0.30 client)";
+                            break;
+                    }
+                } 
                     
                List<Player> usingClient;
-               if (!clients.TryGetValue(appName, out usingClient)) {
+               if (!clients.TryGetValue(appName, out usingClient)) 
+               {
                     usingClient = new List<Player>();
                     clients[appName] = usingClient;
-                }
-                usingClient.Add(pl);
+               }
+               usingClient.Add(pl);
             }
             
             p.Message("Players using:");
-            foreach (var kvp in clients) {
+            foreach (var kvp in clients) 
+            {
                 StringBuilder builder = new StringBuilder();
                 List<Player> players  = kvp.Value;
                 
-                for (int i = 0; i < players.Count; i++) {
+                for (int i = 0; i < players.Count; i++) 
+                {
                     string nick = Colors.StripUsed(p.FormatNick(players[i]));
                     builder.Append(nick);
                     if (i < players.Count - 1) builder.Append(", ");
@@ -57,7 +83,8 @@ namespace MCGalaxy.Commands.Info {
             }
         }
 
-        public override void Help(Player p) {
+        public override void Help(Player p) 
+        {
             p.Message("&T/PClients");
             p.Message("&HLists the clients players are using, and who uses which client.");
         }
