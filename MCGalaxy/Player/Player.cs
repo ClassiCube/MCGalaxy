922425a7f01a4ec0a205ebc2b815bddb3bc85641
UnknownShadow200 <unknownshadow200@gmail.com>
2021-10-13 23:08:55 +1100

    Server.FormatName -> Server.ToRawUsername

--- MCGalaxy/Player/Player.cs
+++ MCGalaxy/Player/Player.cs
@@ -383,7 +383,7 @@ namespace MCGalaxy {
             // TODO: select color from database?
             if (target != null && CanSee(target)) return FormatNick(target);
             
-            return Group.GroupIn(name).Color + Server.FormatName(name);
+            return Group.GroupIn(name).Color + Server.ToRawUsername(name);
         }
 
         /// <summary> Formats a player's name for displaying in chat. </summary>        
