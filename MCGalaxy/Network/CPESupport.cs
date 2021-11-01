4e5a977cea2cd8d21cd1ca550f9ee70eb97e6aa0
UnknownShadow200 <unknownshadow200@gmail.com>
2021-11-01 22:18:30 +1100

    Split up Player.CPE.cs into CPESupport.cs and Player.CPE.cs

---
+++ MCGalaxy/Network/CPESupport.cs
@@ -0,0 +1,95 @@
+﻿/*
+    Copyright 2015 MCGalaxy
+        
+    Dual-licensed under the Educational Community License, Version 2.0 and
+    the GNU General Public License, Version 3 (the "Licenses"); you may
+    not use this file except in compliance with the Licenses. You may
+    obtain a copy of the Licenses at
+    
+    http://www.opensource.org/licenses/ecl2.php
+    http://www.gnu.org/licenses/gpl-3.0.html
+    
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the Licenses are distributed on an "AS IS"
+    BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    or implied. See the Licenses for the specific language governing
+    permissions and limitations under the Licenses.
+ */
+using System;
+using MCGalaxy.Network;
+
+namespace MCGalaxy 
+{
+    public sealed class CpeExtension 
+    {
+        public string Name;
+        public byte ClientVersion, ServerVersion = 1;
+        
+        public CpeExtension(string name) { Name = name; }
+        public CpeExtension(string name, byte version) {
+            Name = name; ServerVersion = version;
+        }
+    }
+    
+    public static class CpeExt 
+    {
+        public const string ClickDistance = "ClickDistance";
+        public const string CustomBlocks = "CustomBlocks";
+        public const string HeldBlock = "HeldBlock";
+        public const string TextHotkey = "TextHotKey";
+        public const string ExtPlayerList = "ExtPlayerList";
+        public const string EnvColors = "EnvColors";
+        public const string SelectionCuboid = "SelectionCuboid";
+        public const string BlockPermissions = "BlockPermissions";
+        public const string ChangeModel = "ChangeModel";
+        public const string EnvMapAppearance = "EnvMapAppearance";
+        public const string EnvWeatherType = "EnvWeatherType";
+        public const string HackControl = "HackControl";
+        public const string EmoteFix = "EmoteFix";
+        public const string MessageTypes = "MessageTypes";
+        public const string LongerMessages = "LongerMessages";
+        public const string FullCP437 = "FullCP437";
+        public const string BlockDefinitions = "BlockDefinitions";
+        public const string BlockDefinitionsExt = "BlockDefinitionsExt";
+        public const string TextColors = "TextColors";
+        public const string BulkBlockUpdate = "BulkBlockUpdate";
+        public const string EnvMapAspect = "EnvMapAspect";
+        public const string PlayerClick = "PlayerClick";
+        public const string EntityProperty = "EntityProperty";
+        public const string ExtEntityPositions = "ExtEntityPositions";
+        public const string TwoWayPing = "TwoWayPing";
+        public const string InventoryOrder = "InventoryOrder";
+        public const string InstantMOTD = "InstantMOTD";
+        public const string FastMap = "FastMap";
+        public const string ExtBlocks = "ExtendedBlocks";
+        public const string ExtTextures = "ExtendedTextures";
+        public const string SetHotbar = "SetHotbar";
+        public const string SetSpawnpoint = "SetSpawnpoint";
+        public const string VelocityControl = "VelocityControl";
+        public const string CustomParticles = "CustomParticles";
+        public const string CustomModels = "CustomModels";
+    }
+    
+    public enum CpeMessageType : byte 
+    {
+        Normal = 0, Status1 = 1, Status2 = 2, Status3 = 3,
+        BottomRight1 = 11, BottomRight2 = 12, BottomRight3 = 13,
+        Announcement = 100, BigAnnouncement = 101, SmallAnnouncement = 102 
+    }
+    
+    public enum EnvProp : byte 
+    {
+        SidesBlock = 0, EdgeBlock = 1, EdgeLevel = 2,
+        CloudsLevel = 3, MaxFog = 4, CloudsSpeed = 5,
+        WeatherSpeed = 6, WeatherFade = 7, ExpFog = 8,
+        SidesOffset = 9, SkyboxHorSpeed = 10, SkyboxVerSpeed = 11,
+        
+        Max,
+        Weather = 255, // this is internal, not an official env prop
+    }
+    
+    public enum EntityProp : byte 
+    {
+        RotX = 0, RotY = 1, RotZ = 2, ScaleX = 3, ScaleY = 4, ScaleZ = 5,
+    }
+}
--- MCGalaxy/Player/Player.CPE.cs
+++ MCGalaxy/Player/Player.CPE.cs
@@ -21,17 +21,6 @@ using BlockID = System.UInt16;
 
 namespace MCGalaxy 
 {
-    public sealed class CpeExtension 
-    {
-        public string Name;
-        public byte ClientVersion, ServerVersion = 1;
-        
-        public CpeExtension(string name) { Name = name; }
-        public CpeExtension(string name, byte version) {
-            Name = name; ServerVersion = version;
-        }
-    }
-    
     public partial class Player 
     {
         public bool hasCpe, finishedCpeLogin;
@@ -227,63 +216,5 @@ namespace MCGalaxy
             }
             Send(bulk);
         }
-    }
-    
-    public static class CpeExt {
-        public const string ClickDistance = "ClickDistance";
-        public const string CustomBlocks = "CustomBlocks";
-        public const string HeldBlock = "HeldBlock";
-        public const string TextHotkey = "TextHotKey";
-        public const string ExtPlayerList = "ExtPlayerList";
-        public const string EnvColors = "EnvColors";
-        public const string SelectionCuboid = "SelectionCuboid";
-        public const string BlockPermissions = "BlockPermissions";
-        public const string ChangeModel = "ChangeModel";
-        public const string EnvMapAppearance = "EnvMapAppearance";
-        public const string EnvWeatherType = "EnvWeatherType";
-        public const string HackControl = "HackControl";
-        public const string EmoteFix = "EmoteFix";
-        public const string MessageTypes = "MessageTypes";
-        public const string LongerMessages = "LongerMessages";
-        public const string FullCP437 = "FullCP437";
-        public const string BlockDefinitions = "BlockDefinitions";
-        public const string BlockDefinitionsExt = "BlockDefinitionsExt";
-        public const string TextColors = "TextColors";
-        public const string BulkBlockUpdate = "BulkBlockUpdate";
-        public const string EnvMapAspect = "EnvMapAspect";
-        public const string PlayerClick = "PlayerClick";
-        public const string EntityProperty = "EntityProperty";
-        public const string ExtEntityPositions = "ExtEntityPositions";
-        public const string TwoWayPing = "TwoWayPing";
-        public const string InventoryOrder = "InventoryOrder";
-        public const string InstantMOTD = "InstantMOTD";
-        public const string FastMap = "FastMap";
-        public const string ExtBlocks = "ExtendedBlocks";
-        public const string ExtTextures = "ExtendedTextures";
-        public const string SetHotbar = "SetHotbar";
-        public const string SetSpawnpoint = "SetSpawnpoint";
-        public const string VelocityControl = "VelocityControl";
-        public const string CustomParticles = "CustomParticles";
-        public const string CustomModels = "CustomModels";
-    }
-    
-    public enum CpeMessageType : byte {
-        Normal = 0, Status1 = 1, Status2 = 2, Status3 = 3,
-        BottomRight1 = 11, BottomRight2 = 12, BottomRight3 = 13,
-        Announcement = 100, BigAnnouncement = 101, SmallAnnouncement = 102 
-    }
-    
-    public enum EnvProp : byte {
-        SidesBlock = 0, EdgeBlock = 1, EdgeLevel = 2,
-        CloudsLevel = 3, MaxFog = 4, CloudsSpeed = 5,
-        WeatherSpeed = 6, WeatherFade = 7, ExpFog = 8,
-        SidesOffset = 9, SkyboxHorSpeed = 10, SkyboxVerSpeed = 11,
-        
-        Max,
-        Weather = 255, // this is internal, not an official env prop
-    }
-    
-    public enum EntityProp : byte {
-        RotX = 0, RotY = 1, RotZ = 2, ScaleX = 3, ScaleY = 4, ScaleZ = 5,
     }
 }
