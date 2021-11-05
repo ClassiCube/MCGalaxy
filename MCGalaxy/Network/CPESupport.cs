ce81eb6a58fe92ee99d2f2313f576f076e945ebd
UnknownShadow200 <unknownshadow200@gmail.com>
2021-11-06 09:39:46 +1100

    Add short description for CPE extensions

--- MCGalaxy/Network/CPESupport.cs
+++ MCGalaxy/Network/CPESupport.cs
@@ -47,10 +47,13 @@ namespace MCGalaxy
     
     public class CpeExt 
     {
-    	public string Name;
-    	public byte ServerVersion;
-    	public byte ClientVersion;
-    	
+        /// <summary> Name of the CPE extension (e.g. ExtPlayerList) </summary>
+        public string Name;
+        /// <summary> Highest version of this CPE extension supported by the server </summary>
+        public byte ServerVersion;
+        /// <summary> Highest version of this CPE extension supported by the client </summary>
+        public byte ClientVersion;
+        
         public const string ClickDistance = "ClickDistance";
         public const string CustomBlocks = "CustomBlocks";
         public const string HeldBlock = "HeldBlock";
@@ -90,55 +93,61 @@ namespace MCGalaxy
     
     public sealed class CpeExtension 
     {
+    	/// <summary> Name of the CPE extension (e.g. ExtPlayerList) </summary>
         public string Name;
-        public byte Version = 1;
+        /// <summary> Short description of this CPE extension </summary>
         public string Desc;
+        /// <summary> Highest version of this CPE extension supported by the server </summary>
+        public byte Version;
+        /// <summary> Whether this CPE extension is currently enabled by the server </summary>
         public bool Enabled;
         
-        public CpeExtension(string name) { Name = name; }
-        public CpeExtension(string name, byte version) {
-            Name = name; Version = version;
+        public CpeExtension(string name, string desc) {
+            Name = name; Desc = desc; Version = 1;
+        }
+        public CpeExtension(string name, string desc, byte version) {
+            Name = name; Desc = desc; Version = version;
         }
         
         
         /// <summary> Array of all supported CPE extensions </summary>
         public static CpeExtension[] All = new CpeExtension[] {
-            new CpeExtension(CpeExt.ClickDistance),    
-            new CpeExtension(CpeExt.CustomBlocks),
-            new CpeExtension(CpeExt.HeldBlock),        
-            new CpeExtension(CpeExt.TextHotkey),
-            new CpeExtension(CpeExt.ExtPlayerList, 2), 
-            new CpeExtension(CpeExt.EnvColors),
-            new CpeExtension(CpeExt.SelectionCuboid),  
-            new CpeExtension(CpeExt.BlockPermissions),
-            new CpeExtension(CpeExt.ChangeModel),      
-            new CpeExtension(CpeExt.EnvMapAppearance, 2),
-            new CpeExtension(CpeExt.EnvWeatherType),   
-            new CpeExtension(CpeExt.HackControl),
-            new CpeExtension(CpeExt.EmoteFix),         
-            new CpeExtension(CpeExt.MessageTypes),
-            new CpeExtension(CpeExt.LongerMessages),   
-            new CpeExtension(CpeExt.FullCP437),
-            new CpeExtension(CpeExt.BlockDefinitions), 
-            new CpeExtension(CpeExt.BlockDefinitionsExt, 2),
-            new CpeExtension(CpeExt.TextColors),       
-            new CpeExtension(CpeExt.BulkBlockUpdate),
-            new CpeExtension(CpeExt.EnvMapAspect),     
-            new CpeExtension(CpeExt.PlayerClick),
-            new CpeExtension(CpeExt.EntityProperty),   
-            new CpeExtension(CpeExt.ExtEntityPositions),
-            new CpeExtension(CpeExt.TwoWayPing),       
-            new CpeExtension(CpeExt.InventoryOrder),
-            new CpeExtension(CpeExt.InstantMOTD),      
-            new CpeExtension(CpeExt.FastMap),
-            new CpeExtension(CpeExt.ExtTextures),      
-            new CpeExtension(CpeExt.SetHotbar),
-            new CpeExtension(CpeExt.SetSpawnpoint),    
-            new CpeExtension(CpeExt.VelocityControl),
-            new CpeExtension(CpeExt.CustomParticles),  
-            new CpeExtension(CpeExt.CustomModels, 2),
+            new CpeExtension(CpeExt.ClickDistance,       "Allows controlling how far away blocks can be placed/deleted (/Reach)"),    
+            new CpeExtension(CpeExt.CustomBlocks,        "Allows showing blocks 50 - 65 (Cobblestone Slab - Stone Brick)"),
+            new CpeExtension(CpeExt.HeldBlock,           "Allows setting currently held block/block in hand"),
+            new CpeExtension(CpeExt.TextHotkey,          "Allows defining custom hotkeys"),
+            new CpeExtension(CpeExt.ExtPlayerList,       "Allows separating tab list from entities in current map", 2), 
+            new CpeExtension(CpeExt.EnvColors,           "Allows customing environment colors such as sky color (/Env)"),
+            new CpeExtension(CpeExt.SelectionCuboid,     "Allows showing colored boxes in the map (/Zone set"),
+            new CpeExtension(CpeExt.BlockPermissions,    "Allows controlling which individual blocks can be placed/deleted"),
+            new CpeExtension(CpeExt.ChangeModel,         "Allows changing models of entities (/Model)"),      
+            new CpeExtension(CpeExt.EnvMapAppearance,    "Allows customising map appearance (/Texture)", 2),
+            new CpeExtension(CpeExt.EnvWeatherType,      "Allows customising weather (/Env)"),
+            new CpeExtension(CpeExt.HackControl,         "Allows changing hacks permissions (e.g. speed) on the fly"), 
+            new CpeExtension(CpeExt.EmoteFix,            "Allows clients to properly render emotes"),         
+            new CpeExtension(CpeExt.MessageTypes,        "Allows messages to appear in special locations (e.g. top right)"),
+            new CpeExtension(CpeExt.LongerMessages,      "Allows typing multiline chat in clients"),   
+            new CpeExtension(CpeExt.FullCP437,           "Allows using all code page 437 characters in chat"),
+            new CpeExtension(CpeExt.BlockDefinitions,    "Allows defining custom blocks (/LB and /GB)"),
+            new CpeExtension(CpeExt.BlockDefinitionsExt, "Allows defining custom blocks with more flexible shapes (/LB and /GB)", 2),
+            new CpeExtension(CpeExt.TextColors,          "Allows customising chat color codes (/CustomColors)"),   
+            new CpeExtension(CpeExt.BulkBlockUpdate,     "Allows sending block updates in a faster way"),
+            new CpeExtension(CpeExt.EnvMapAspect,        "Allows customising map appearance (/Env and /Texture)"),
+            new CpeExtension(CpeExt.PlayerClick,         "Allows retrieving precise details on mouse clicks"),
+            new CpeExtension(CpeExt.EntityProperty,      "Allows customising properties of entities (/EntityRot)"),   
+            new CpeExtension(CpeExt.ExtEntityPositions,  "Allows entities to appear at positions past 1023"),
+            new CpeExtension(CpeExt.TwoWayPing,          "Allows measuring ping (/Ping)"),       
+            new CpeExtension(CpeExt.InventoryOrder,      "Allows configuring order of blocks in Inventory menu"),
+            new CpeExtension(CpeExt.InstantMOTD,         "Allows sending MOTD packets without also needing to resend map"),      
+            new CpeExtension(CpeExt.FastMap,             "Allows sending maps in a faster way"),
+            new CpeExtension(CpeExt.ExtTextures,         "Allows using texture IDs over 255 in block definitions"),
+            new CpeExtension(CpeExt.SetHotbar,           "Allows setting blocks in hotbar (the bar with 9 blocks)"),
+            new CpeExtension(CpeExt.SetSpawnpoint,       "Allows changing spawn point of players without teleporting them"),    
+            new CpeExtension(CpeExt.VelocityControl,     "Allows adjusting velocity of players"),
+            new CpeExtension(CpeExt.CustomParticles,     "Allows defining and spawning custom particles"),
+            new CpeExtension(CpeExt.CustomModels,        "Allows defining custom models for entities", 2),
             #if TEN_BIT_BLOCKS
-            new CpeExtension(CpeExt.ExtBlocks),
+            new CpeExtension(CpeExt.ExtBlocks,           "Allows using block IDs over 255 in block definitions"),
             #endif
         };
         internal static CpeExt[] Empty = new CpeExt[0];
