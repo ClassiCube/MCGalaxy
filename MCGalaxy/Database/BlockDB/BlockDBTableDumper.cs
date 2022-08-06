ClassiCube/MCGalaxy

commit f1a8544308094586a3a1b5d61cd96d03391ad448

Rewrite to avoid DataTable completely

MCGalaxy/Database/BlockDB/BlockDBTableDumper.cs
@@ -45,7 +45,7 @@ namespace MCGalaxy.DB {
             mapName = table.Substring("Block".Length);
             
             try {
-                Database.Backend.IterateRows(table, "*", null, DumpRow);
+                Database.Backend.ReadRows(table, "*", null, DumpRow);
                 WriteBuffer(true);
                 AppendCbdbFile();
                 SaveCbdbFile();

MCGalaxy/Levels/LevelDB.cs
@@ -41,73 +41,76 @@ namespace MCGalaxy {
             Logger.Log(LogType.BackgroundActivity, "Saved BlockDB changes for: {0}", lvl.name);
         }
 
-        internal static void LoadZones(Level level, string name) {
-            if (!Database.TableExists("Zone" + name)) return;
-            int id = 0;
-            bool changedPerbuild = false;
+        static object ListZones(IDataRecord record, object arg) {
+            Zone z = new Zone();
+            z.MinX = (ushort)record.GetInt32("SmallX");
+            z.MinY = (ushort)record.GetInt32("SmallY");
+            z.MinX = (ushort)record.GetInt32("SmallZ");
             
-            using (DataTable table = Database.Backend.GetRows("Zone" + name, "*")) {
-                foreach (DataRow row in table.Rows) {
-                    Zone z = new Zone(level);
-                    z.MinX = ushort.Parse(row["SmallX"].ToString());
-                    z.MinY = ushort.Parse(row["SmallY"].ToString());
-                    z.MinZ = ushort.Parse(row["SmallZ"].ToString());
-                    z.MaxX = ushort.Parse(row["BigX"].ToString());
-                    z.MaxY = ushort.Parse(row["BigY"].ToString());
-                    z.MaxZ = ushort.Parse(row["BigZ"].ToString());
-                    
-                    string owner = row["Owner"].ToString();
-                    if (owner.StartsWith("grp")) {
-                        Group grp = Group.Find(owner.Substring(3));
-                        if (grp != null) z.Access.Min = grp.Permission;
-                    } else if (z.CoversMap(level)) {
-                        level.BuildAccess.Whitelisted.Add(owner);
-                        changedPerbuild = true;
-                        continue;
-                    } else {
-                        z.Access.Whitelisted.Add(owner);
-                        z.Access.Min = LevelPermission.Admin;
-                    }
-                    
-                    z.Config.Name = "Zone" + id;
-                    id++;
-                    z.AddTo(level);
+            z.MaxX = (ushort)record.GetInt32("BigX");
+            z.MaxY = (ushort)record.GetInt32("BigY");
+            z.MaxX = (ushort)record.GetInt32("BigZ");
+            z.Config.Name = record.GetString("Owner");
+            
+            ((List<Zone>)arg).Add(z);
+            return arg;
+        }
+        
+        internal static void LoadZones(Level level, string map) {
+            if (!Database.TableExists("Zone" + map)) return;
+            
+            List<Zone> zones = new List<Zone>();
+            Database.Backend.ReadRows("Zone" + map, "*", zones, ListZones);
+            
+            bool changedPerbuild = false;
+            for (int i = 0; i < zones.Count; i++) {
+                Zone z = zones[i];
+                string owner = z.Config.Name;
+                
+                if (owner.StartsWith("grp")) {
+                    Group grp = Group.Find(owner.Substring(3));
+                    if (grp != null) z.Access.Min = grp.Permission;
+                } else if (z.CoversMap(level)) {
+                    level.BuildAccess.Whitelisted.Add(owner);
+                    changedPerbuild = true;
+                    continue;
+                } else {
+                    z.Access.Whitelisted.Add(owner);
+                    z.Access.Min = LevelPermission.Admin;
                 }
+                
+                z.Config.Name = "Zone" + i;
+                z.AddTo(level);
             }
             
             if (changedPerbuild) Level.SaveSettings(level);
             if (level.Zones.Count > 0 && !level.Save(true)) return;
-            Database.Backend.DeleteTable("Zone" + name);
-            Logger.Log(LogType.SystemActivity, "Upgraded zones for map " + name);
+            
+            Database.Backend.DeleteTable("Zone" + map);
+            Logger.Log(LogType.SystemActivity, "Upgraded zones for map " + map);
         }
         
-        internal static void LoadPortals(Level level, string name) {
-            level.hasPortals = Database.TableExists("Portals" + name);
+        internal static void LoadPortals(Level level, string map) {
+            level.hasPortals = Database.TableExists("Portals" + map);
             if (!level.hasPortals) return;
+            List<Vec3U16> coords = Portal.GetAllCoords(map);
             
-            using (DataTable table = Database.Backend.GetRows("Portals" + name, "*")) {
-                foreach (DataRow row in table.Rows) {
-                    ushort x = ushort.Parse(row["EntryX"].ToString());
-                    ushort y = ushort.Parse(row["EntryY"].ToString());
-                    ushort z = ushort.Parse(row["EntryZ"].ToString());
-                    
-                    BlockID block = level.GetBlock(x, y, z);
-                    if (level.Props[block].IsPortal) continue;
-                    
-                    Database.Backend.DeleteRows("Portals" + name, "WHERE EntryX=@0 AND EntryY=@1 AND EntryZ=@2", x, y, z);
-                }
+            foreach (Vec3U16 p in coords) {
+                BlockID block = level.GetBlock(p.X, p.Y, p.Z);
+                if (level.Props[block].IsPortal) continue;
+                Portal.Delete(map, p.X, p.Y, p.Z);
             }
         }
         
-        internal static void LoadMessages(Level level, string name) {
-            level.hasMessageBlocks = Database.TableExists("Messages" + name);
-            if (!level.hasMessageBlocks) return;            
-            List<Vec3U16> coords = MessageBlock.GetAll(name);
+        internal static void LoadMessages(Level level, string map) {
+            level.hasMessageBlocks = Database.TableExists("Messages" + map);
+            if (!level.hasMessageBlocks) return;
+            List<Vec3U16> coords = MessageBlock.GetAllCoords(map);
             
             foreach (Vec3U16 p in coords) {
                 BlockID block = level.GetBlock(p.X, p.Y, p.Z);
                 if (level.Props[block].IsMessageBlock) continue;
-                MessageBlock.Delete(name, p.X, p.Y, p.Z);
+                MessageBlock.Delete(map, p.X, p.Y, p.Z);
             }
         }
         

MCGalaxy/Blocks/Extended/MessageBlock.cs
@@ -98,26 +98,15 @@ namespace MCGalaxy.Blocks.Extended {
             }
         }
 
-                
-        static object IterateAll(IDataRecord record, object arg) {
-            Vec3U16 pos;
-            pos.X = (ushort)record.GetInt32(0);
-            pos.Y = (ushort)record.GetInt32(1);
-            pos.Z = (ushort)record.GetInt32(2);
-            
-            ((List<Vec3U16>)arg).Add(pos);
-            return arg;
-        }
-        
-        internal static List<Vec3U16> GetAll(string map) {
+
+        internal static List<Vec3U16> GetAllCoords(string map) {
             List<Vec3U16> coords = new List<Vec3U16>();
-            Database.Backend.IterateRows("Messages" + map, "X,Y,Z",
-                                         coords, IterateAll);
+            Database.Backend.ReadRows("Messages" + map, "X,Y,Z", coords, Portal.ReadCoords);
             return coords;
         }
         
         internal static string Get(string map, ushort x, ushort y, ushort z) {
-            string msg = Database.GetString("Messages" + map, "Message",
+            string msg = Database.ReadString("Messages" + map, "Message",
                                             "WHERE X=@0 AND Y=@1 AND Z=@2", x, y, z);
             if (msg == null) return null;
 

MCGalaxy/Blocks/Extended/Portal.cs
@@ -16,38 +16,20 @@
     permissions and limitations under the Licenses.
  */
 using System;
+using System.Collections.Generic;
 using System.Data;
+using MCGalaxy.Maths;
 using MCGalaxy.SQL;
 
 namespace MCGalaxy.Blocks.Extended {
+    public class PortalExit { public string Map; public ushort X, Y, Z; }
+    
     public static class Portal {
-        
-        public class PortalExitData {
-            public string Map;
-            public int X, Y, Z;
-        }
-        
-        static object IteratePortalExit(IDataRecord record, object arg) {
-            PortalExitData data = new PortalExitData();
-            data.Map = record.GetString("ExitMap");
-            data.X   = record.GetInt32("ExitX");
-            data.Y   = record.GetInt32("ExitY");
-            data.Z   = record.GetInt32("ExitZ");
-            
-            data.Map = data.Map.Cp437ToUnicode();
-            return data;
-        }
-        
-        public static PortalExitData Get(string map, ushort x, ushort y, ushort z) {
-            object raw = Database.Backend.IterateRows("Portals" + map, "*", null, IteratePortalExit,
-			                                          "WHERE EntryX=@0 AND EntryY=@1 AND EntryZ=@2", x, y, z);
-		    return (PortalExitData)raw;
-        }
         
         public static bool Handle(Player p, ushort x, ushort y, ushort z) {
             if (!p.level.hasPortals) return false;
             
-            PortalExitData exit = Get(p.level.MapName, x, y, z);
+            PortalExit exit = Get(p.level.MapName, x, y, z);
             if (exit == null) return false;
             Orientation rot = p.Rot;
             
@@ -63,7 +45,7 @@ namespace MCGalaxy.Blocks.Extended {
                     changedMap = false;
                 }
                 
-                p.summonedMap = null;                
+                p.summonedMap = null;
                 if (!changedMap) { Player.Message(p, "Unable to use this portal, as this portal goes to that map."); return true; }
                 p.BlockUntilLoad(10);
             }
@@ -71,6 +53,57 @@ namespace MCGalaxy.Blocks.Extended {
             Position pos = Position.FromFeetBlockCoords(exit.X, exit.Y, exit.Z);
             p.SendPos(Entities.SelfID, pos, rot);
             return true;
+        }
+        
+        internal static object ReadCoords(IDataRecord record, object arg) {
+            Vec3U16 pos;
+            pos.X = (ushort)record.GetInt32(0);
+            pos.Y = (ushort)record.GetInt32(1);
+            pos.Z = (ushort)record.GetInt32(2);
+            
+            ((List<Vec3U16>)arg).Add(pos);
+            return arg;
+        }
+        
+        internal static List<Vec3U16> GetAllCoords(string map) {
+            List<Vec3U16> coords = new List<Vec3U16>();
+            Database.Backend.ReadRows("Portals" + map, "EntryX,EntryY,EntryZ", coords, ReadCoords);
+            return coords;
+        }
+        
+        static PortalExit ParseExit(IDataRecord record) {
+            PortalExit data = new PortalExit();
+            data.Map = record.GetString(0).Cp437ToUnicode();
+            
+            data.X = (ushort)record.GetInt32(1);
+            data.Y = (ushort)record.GetInt32(2);
+            data.Z = (ushort)record.GetInt32(3);
+            return data;
+        }
+        
+        static object ReadAllExits(IDataRecord record, object arg) {
+            ((List<PortalExit>)arg).Add(ParseExit(record));
+            return arg;
+        }
+        
+        public static List<PortalExit> GetAll(string map) {
+            List<PortalExit> exits = new List<PortalExit>();
+            Database.Backend.ReadRows("Portals" + map, "ExitMap,ExitX,ExitY,ExitZ",
+                                      exits, ReadAllExits);
+            return exits;
+        }
+        
+        static object ReadExit(IDataRecord record, object arg) { return ParseExit(record); }
+        public static PortalExit Get(string map, ushort x, ushort y, ushort z) {
+            object raw = Database.Backend.ReadRows("Portals" + map, "ExitMap,ExitX,ExitY,ExitZ",
+                                                   null, ReadExit,
+                                                   "WHERE EntryX=@0 AND EntryY=@1 AND EntryZ=@2", x, y, z);
+            return (PortalExit)raw;
+        }
+        
+        internal static void Delete(string map, ushort x, ushort y, ushort z) {
+            Database.Backend.DeleteRows("Portals" + map,
+                                        "WHERE EntryX=@0 AND EntryY=@1 AND EntryZ=@2", x, y, z);
         }
     }
 }

MCGalaxy/Commands/Chat/CmdColor.cs
@@ -16,7 +16,7 @@
     permissions and limitations under the Licenses.
  */
 using MCGalaxy.Bots;
-using MCGalaxy.SQL;
+using MCGalaxy.DB;
 
 namespace MCGalaxy.Commands.Chatting {    
     public class CmdColor : EntityPropertyCmd {
@@ -63,7 +63,7 @@ namespace MCGalaxy.Commands.Chatting {
             
             Entities.GlobalRespawn(who);
             who.SetPrefix();
-            Database.Backend.UpdateRows("Players", "color = @1", "WHERE Name = @0", who.name, color);
+            PlayerData.Update(who.name, PlayerData.ColumnColor, color);
         }
         
         public override void Help(Player p) {

MCGalaxy/Commands/Chat/CmdInbox.cs
@@ -27,7 +27,7 @@ namespace MCGalaxy.Commands.Chatting {
         public override bool SuperUseable { get { return false; } }
 
         class MailEntry { public string Contents, Timestamp, From; }
-        static object IterateInbox(IDataRecord record, object arg) {
+        static object ReadInbox(IDataRecord record, object arg) {
             MailEntry e = new MailEntry();
             e.Contents  = record.GetString("Contents");
             e.Timestamp = record.GetString("TimeSent");
@@ -38,8 +38,8 @@ namespace MCGalaxy.Commands.Chatting {
         
         public override void Use(Player p, string message) {
             List<MailEntry> entries = new List<MailEntry>();
-            Database.Backend.IterateRows("Inbox" + p.name, "*",
-                                         entries, IterateInbox, "ORDER BY TimeSent");
+            Database.Backend.ReadRows("Inbox" + p.name, "*",
+                                      entries, ReadInbox, "ORDER BY TimeSent");
             if (entries.Count == 0) {
                 Player.Message(p, "Your inbox is empty."); return;
             }
@@ -87,7 +87,7 @@ namespace MCGalaxy.Commands.Chatting {
         }
         
         static void Output(Player p, MailEntry entry) {
-            TimeSpan delta = DateTime.Now - Convert.ToDateTime(entry.Timestamp);
+            TimeSpan delta = DateTime.Now - DateTime.Parse(entry.Timestamp);
             string sender = PlayerInfo.GetColoredName(p, entry.From);
             
             Player.Message(p, "From {0} &a{1} ago:", sender, delta.Shorten());

MCGalaxy/Commands/Chat/CmdTColor.cs
@@ -15,7 +15,7 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
-using MCGalaxy.SQL;
+using MCGalaxy.DB;
 
 namespace MCGalaxy.Commands.Chatting {
     public class CmdTColor : EntityPropertyCmd {
@@ -44,7 +44,7 @@ namespace MCGalaxy.Commands.Chatting {
             
             who.titlecolor = color;
             who.SetPrefix();
-            Database.Backend.UpdateRows("Players", "title_color = @1", "WHERE Name = @0", who.name, color);
+            PlayerData.Update(who.name, PlayerData.ColumnTColor, color);
         }
 
         public override void Help(Player p) {

MCGalaxy/Commands/Chat/CmdTitle.cs
@@ -15,7 +15,8 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
-using MCGalaxy.SQL;
+using MCGalaxy.DB;
+
 namespace MCGalaxy.Commands.Chatting {    
     public class CmdTitle : EntityPropertyCmd {        
         public override string name { get { return "Title"; } }
@@ -44,7 +45,7 @@ namespace MCGalaxy.Commands.Chatting {
             
             who.title = title;
             who.SetPrefix();
-            Database.Backend.UpdateRows("Players", "Title = @1", "WHERE Name = @0", who.name, title.UnicodeToCp437());
+            PlayerData.Update(who.name, PlayerData.ColumnTitle, title.UnicodeToCp437());
         }
         
         public override void Help(Player p) {

commit 29a1b06301ea22d391df5e82c05ace198d1b54c5

Fix old database migration bugs
In detail:
1) BlockDB upgrader didn't convert time from local to UTC, which meant that times in /b were e.g. 10 hours off
2) Zone migrator accidentally set minX/maxX to imported minZ/maxZ and left minZ/maxZ as 0. Bug introduced four years ago in f1a8544308094586a3a1b5d61cd96d03391ad448

MCGalaxy/Database/BlockDB/BlockDBTableDumper.cs
@@ -169,7 +169,8 @@ namespace MCGalaxy.DB {
             int sec =   (date[17] - '0') * 10  + (date[18] - '0');
             
             DateTime time = new DateTime(year, month, day, hour, min, sec);
-            entry.TimeDelta = (int)time.Subtract(BlockDB.Epoch).TotalSeconds;
+            DateTime utc  = time.ToUniversalTime();
+            entry.TimeDelta = (int)utc.Subtract(BlockDB.Epoch).TotalSeconds;
         }
     }
 }

MCGalaxy/Levels/LevelDB.cs
@@ -44,11 +44,11 @@ namespace MCGalaxy {
             Zone z = new Zone();
             z.MinX = (ushort)record.GetInt("SmallX");
             z.MinY = (ushort)record.GetInt("SmallY");
-            z.MinX = (ushort)record.GetInt("SmallZ");
+            z.MinZ = (ushort)record.GetInt("SmallZ");
             
             z.MaxX = (ushort)record.GetInt("BigX");
             z.MaxY = (ushort)record.GetInt("BigY");
-            z.MaxX = (ushort)record.GetInt("BigZ");
+            z.MaxZ = (ushort)record.GetInt("BigZ");
             z.Config.Name = record.GetText("Owner");
             return z;
         }