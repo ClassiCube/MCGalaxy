4e9f9c754ac0c36cdff0700b3768989d9a9ff451
UnknownShadow200 <unknownshadow200@gmail.com>
2022-08-18 21:28:27 +1000

    add ImageSharp license

--- MCGalaxy/Events/IPluginEvent.cs
+++ MCGalaxy/Events/IPluginEvent.cs
@@ -16,7 +16,6 @@
     permissions and limitations under the Licenses.
  */
 using System;
-using System.Collections.Generic;
 
 // kept in this namespace for backwards compatbility
 namespace MCGalaxy 
