f306d9284a4a1156301dbff7f2dffed70debfe39
UnknownShadow200 <unknownshadow200@gmail.com>
2022-10-04 20:04:10 +1100

    Fix rotate output being wrong for angles > 90

--- MCGalaxy/Drawing/Transform/RotateTransform.cs
+++ MCGalaxy/Drawing/Transform/RotateTransform.cs
@@ -26,31 +26,63 @@ namespace MCGalaxy.Drawing.Transforms
     {        
         public override string Name { get { return "Rotate"; } }
         public bool CentreOrigin;
-        double alphaX, betaX, alphaY, betaY, alphaZ, betaZ;
+        Shear2D shearX, shearY, shearZ;
         Vec3S32 P;
+
+        struct Shear2D {
+            public int xMulX, xMulY, yMulX, yMulY;
+            public double alpha, beta;
+        };
         
         // https://silmon.github.io/arbitrary-image-rotation-using-shearing.html
         // http://delta.cs.cinvestav.mx/~mcintosh/newweb/camex/node37.html
         // https://www.ocf.berkeley.edu/~fricke/projects/israel/paeth/rotation_by_shearing.html
-        
         public void SetAngles(double xDeg, double yDeg, double zDeg) {
-            CalcShear2D(xDeg, out alphaX, out betaX);
-            CalcShear2D(yDeg, out alphaY, out betaY);
-            CalcShear2D(zDeg, out alphaZ, out betaZ);         
+            CalcShear2D(xDeg, ref shearX);
+            CalcShear2D(yDeg, ref shearY);
+            CalcShear2D(zDeg, ref shearZ);         
         }
         
-        void CalcShear2D(double angle, out double alpha, out double beta) {
-            angle *= -Math.PI / 180.0; // degrees -> radians, and - so same direction as old RotateTransform
-            alpha = -Math.Tan(angle / 2);
-            beta  = Math.Sin(angle);
+        void CalcShear2D(double angle, ref Shear2D shear) {
+            angle %= 360.0;
+            if (angle < 0) angle += 360.0;
+
+            // trying to use shear with angles close to 180 tends to cause issues
+            //  (due to Math.Tan(angle / 2) approaching tan(45 degrees))
+            // so avoid this by reducing the angles to 0-90 degrees,
+            //  then rotating the output appropriately afterwards
+            if (angle >= 0 && angle <= 90) {
+                shear.xMulX =  1;
+                shear.yMulY =  1;
+            } else if (angle > 90 && angle <= 180) {
+                angle -= 90;
+                shear.xMulY =  1;
+                shear.yMulX = -1;
+            } else if (angle > 180 && angle <= 270) {
+                angle -= 180;
+                shear.xMulX = -1;
+                shear.yMulY = -1;
+            } else {
+                angle -= 270;
+                shear.xMulY = -1;
+                shear.yMulX =  1;
+            }
+
+            angle = -angle; // same output as old Rotate Transform
+
+            angle *= Math.PI / 180.0; // degrees -> radians;
+            shear.alpha = -Math.Tan(angle / 2);
+            shear.beta  = Math.Sin(angle);
         }
         
-        void Shear2D(ref int x, ref int y, double alpha, double beta) {
-            int x_ = (int)(x  + alpha * (y  + 0.5)); // shear #1
-            int y_ = (int)(y  + beta  * (x_ + 0.5)); // shear #2
-            int X_ = (int)(x_ + alpha * (y_ + 0.5)); // shear #3
-            
-            x = X_; y = y_;
+        void DoShear2D(ref int x, ref int y, ref Shear2D shear) {
+            int X_ = (int)(x  + shear.alpha * (y  + 0.5)); // shear #1
+            int Y_ = (int)(y  + shear.beta  * (X_ + 0.5)); // shear #2
+            X_     = (int)(X_ + shear.alpha * (Y_ + 0.5)); // shear #3
+
+            // rotate by quadrant the angle was originally in
+            x = shear.xMulX * X_ + shear.xMulY * Y_;
+            y = shear.yMulX * X_ + shear.yMulY * Y_;
         }
         
         public override void Perform(Vec3S32[] marks, DrawOp op, Brush brush, DrawOpOutput output) {
@@ -62,9 +94,9 @@ namespace MCGalaxy.Drawing.Transforms
         void OutputBlock(DrawOpBlock b, DrawOpOutput output) {
             int dx = b.X - P.X, dy = b.Y - P.Y, dz = b.Z - P.Z;
             
-            Shear2D(ref dy, ref dz, alphaX, betaX);
-            Shear2D(ref dx, ref dz, alphaY, betaY);
-            Shear2D(ref dx, ref dy, alphaZ, betaZ);
+            DoShear2D(ref dy, ref dz, ref shearX);
+            DoShear2D(ref dx, ref dz, ref shearY);
+            DoShear2D(ref dx, ref dy, ref shearZ);
             
             b.X = (ushort)(P.X + dx);
             b.Y = (ushort)(P.Y + dy);
