6a80bed166e69bac9d9ace1b4102493b2fb7b09a
Goodlyay <zackxv7@yahoo.com>
2023-05-15 20:22:12 -0700

    Keep binary compatibility for SendCpeMessage

--- MCGalaxy/Network/Player.Networking.cs
+++ MCGalaxy/Network/Player.Networking.cs
@@ -56,6 +56,10 @@ namespace MCGalaxy
             }
         }
 
+        public void SendCpeMessage(CpeMessageType type, string message) {
+            SendCpeMessage(type, message, Chat.PersistentMessage.Priority.Normal);
+        }
+
         public void SendCpeMessage(CpeMessageType type, string message, Chat.PersistentMessage.Priority priority = Chat.PersistentMessage.Priority.Normal) {
             if (type != CpeMessageType.Normal && !Supports(CpeExt.MessageTypes)) {
                 if (type >= CpeMessageType.Announcement) type = CpeMessageType.Normal;
