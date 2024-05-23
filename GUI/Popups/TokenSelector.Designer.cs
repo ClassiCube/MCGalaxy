d4cc9d72f112d8b7d9baf99250f46c63d6b3f75f
Pear <20259871+TheRealPear@users.noreply.github.com>
2024-05-22 23:21:50 -0400

    MCGalaxy GUI refresh

--- GUI/Popups/TokenSelector.Designer.cs
+++ GUI/Popups/TokenSelector.Designer.cs
@@ -7,16 +7,17 @@
         /// not be able to load this method if it was changed manually.
         /// </summary>
         private void InitializeComponent() {
-            this.btnCancel = new System.Windows.Forms.Button();
             this.components = new System.ComponentModel.Container();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // btnCancel
             // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(100, 100);
+            this.btnCancel.Location = new System.Drawing.Point(50, 100);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(100, 25);
             this.btnCancel.TabIndex = 260;
@@ -25,20 +26,22 @@
             // 
             // TokenSelector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(200, 200);
             this.Controls.Add(this.btnCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Load += new System.EventHandler(this.TokenSelector_Load);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "TokenSelector";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ColorPicker";
+            this.Load += new System.EventHandler(this.TokenSelector_Load);
             this.ResumeLayout(false);
+
         }
                 
         protected override void Dispose(bool disposing) {
