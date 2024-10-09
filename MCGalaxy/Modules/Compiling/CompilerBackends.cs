29e205e26f5aec98bf785fa90ccf1f04cbee1694
UnknownShadow200 <unknownshadow200@gmail.com>
2024-10-09 20:58:51 +1100

    Fix for old mono versions

--- MCGalaxy/Modules/Compiling/CompilerBackends.cs
+++ MCGalaxy/Modules/Compiling/CompilerBackends.cs
@@ -193,9 +193,9 @@ namespace MCGalaxy.Modules.Compiling
             string root = RuntimeEnvironment.GetRuntimeDirectory();
             
             string[] paths = new string[] {
-                Path.Combine(root, "csc.exe"), // C# compiler
-                Path.Combine(root, "mcs.exe"), // old Mono C# compiler
-                Path.Combine(root, "mcs"),     // old Mono C# compiler
+                Path.Combine(root, "csc.exe"),     // C# compiler
+                Path.Combine(root, @"../bin/mcs"), // old Mono C# compiler
+                Path.Combine(root, "mcs.exe"),     // old Mono C# compiler
             };
             
             foreach (string path in paths)
